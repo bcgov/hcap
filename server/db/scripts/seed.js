02fb2e5b85378a8264c4961bb99b5044060fe214
mauriciofreshw <mauriciojourdan@Mauricios-MBP.hitronhub.home>
2020-10-08 11:25:07 -0700

    Removed password and improved seed script

--- server/db/scripts/seed.js
+++ server/db/scripts/seed.js
@@ -2,7 +2,7 @@
 const { dbClient } = require('../db.js');
 const { schema } = require('../schema.js');
 
-//Postgres duplicated error code
+// Postgres duplicated error code
 const ERROR_DUPLICATED = '42P07';
 
 // If run directly, will set up local DB
@@ -13,24 +13,35 @@ const ERROR_DUPLICATED = '42P07';
       console.log('Connecting with database server...');
       await dbClient.connect();
 
+      console.log('Creating tables and indexes...');
       for (const schemaItem of schema) {
         try {
           await dbClient.db.createDocumentTable(schemaItem.collection);
+          console.log(`'${schemaItem.collection}' table created.`);
         } catch (err) {
-          if (err.code !== ERROR_DUPLICATED) throw err;
+          if (err.code !== ERROR_DUPLICATED) {
+            throw err;
+          } else {
+            console.log(`'${schemaItem.collection}' table already created, skipping...`);
+          }
         }
         for (const index of schemaItem.indexes) {
           try {
             await dbClient.db.query(index);
+            console.log(`${index} (Success)`);
           } catch (err) {
-            if (err.code !== ERROR_DUPLICATED) throw err;
+            if (err.code !== ERROR_DUPLICATED) {
+              throw err;
+            } else {
+              console.log(`${index} (Already created, skipping...)`);
+            }
           }
         }
       }
       console.log('Done.');
       return process.exit();
     } catch (error) {
-      console.error(`Failed to create collections or indexes, ${error}`);
+      console.error(`Failed to create tables or indexes, ${error}`);
     }
   }
 })();

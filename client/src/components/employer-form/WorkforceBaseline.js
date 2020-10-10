4ade47021bee834a7800b17422ce949f512f48a1
mauriciofreshw <mauriciojourdan@Mauricios-MBP.hitronhub.home>
2020-10-09 17:32:48 -0700

    wip interactive table list

--- client/src/components/employer-form/WorkforceBaseline.js
+++ client/src/components/employer-form/WorkforceBaseline.js
@@ -6,6 +6,7 @@ import { FastField } from 'formik';
 import { Card, Divider } from '../generic';
 import { RenderTextField } from '../fields';
 import { OrgBookSearch } from './OrgBookSearch';
+import { Box } from '@material-ui/core';
 
 export const WorkforceBaseline = ({ isDisabled }) => {
 
@@ -18,30 +19,22 @@ export const WorkforceBaseline = ({ isDisabled }) => {
           </Typography>
           <Divider />
         </Grid>
+
+        <Box pt={1} pb={2} pl={2} pr={2}>
+          <Typography variant="body1">
+            This section collects baseline workforce data for the purpose of supporting equitable allocation of Health Care Support
+            Workers (HCSW). The data will also support efforts to ensure that HCAP is utilized by net new health sector employees.
+          </Typography>
+        </Box>
+
         <Grid item xs={12}>
-        <FastField
+          <FastField
             name="registeredBusinessName"
             component={OrgBookSearch}
             label="* Registered Business Name"
             disabled={isDisabled}
           />
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <FastField
-            name="address"
-            component={RenderTextField}
-            label="* Business address"
-            disabled={isDisabled}
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <FastField
-            name="postalCode"
-            component={RenderTextField}
-            label="* Postal code"
-            disabled={isDisabled}
-          />
-        </Grid>        
       </Grid>
     </Card>
   );

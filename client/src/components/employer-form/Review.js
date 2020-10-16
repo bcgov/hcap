a30158dc51087bdc083a05fbddaa4f5a33a7818f
Kendall Olsen-Maier <kendall@freshworks.io>
2020-10-16 12:12:01 -0700

    Hide instructional box on confirmation screen

--- client/src/components/employer-form/Review.js
+++ client/src/components/employer-form/Review.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
+import { useLocation } from 'react-router-dom'
 import Alert from '@material-ui/lab/Alert';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Box from '@material-ui/core/Box';
 import EditIcon from '@material-ui/icons/Edit';
 
+import { Routes } from '../../constants';
 import { Card, Button } from '../generic';
 import { OperatorInfo } from './OperatorInfo';
 import { SiteInfo } from './SiteInfo';
@@ -13,21 +15,24 @@ import { WorkforceBaseline } from './WorkforceBaseline';
 import { CollectionNotice } from './CollectionNotice';
 
 export const Review = ({ handleEditClick, isDisabled }) => {
+  const location = useLocation();
 
   return (
     <Fragment>
       <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <Box mb={2}>
-            <Alert severity="info">
-              <Typography variant="body2" gutterBottom>
-                <b>
-                  Please review your information for accuracy and completeness.
-                </b>
-              </Typography>
-            </Alert>
-          </Box>
-        </Grid>
+        {!isDisabled && (
+          <Grid item xs={12}>
+            <Box mb={2}>
+              <Alert severity="info">
+                <Typography variant="body2" gutterBottom>
+                  <b>
+                    Please review your information for accuracy and completeness.
+                  </b>
+                </Typography>
+              </Alert>
+            </Box>
+          </Grid>
+        )}
 
         <Grid item xs={12}>
           <Card>
@@ -38,7 +43,7 @@ export const Review = ({ handleEditClick, isDisabled }) => {
                     1. Operator Contact Information
                   </Typography>
                 </Grid>
-                {!isDisabled && (
+                {location.pathname === Routes.EmployerForm && (
                   <Grid item>
                     <Button
                       startIcon={<EditIcon />}

d81c151b75c46d01c69a7cb6673b87c191037d17
Ashton Meuser <ashtonmeuser@gmail.com>
2020-10-30 11:32:31 -0700

    Change order of columns

--- server/scripts/export.js
+++ server/scripts/export.js
@@ -1,9 +1,8 @@
-/* eslint-disable */
-const { dbClient } = require('../db/db.js');
-const { collections } = require('../db/schema.js');
 const csv = require('fast-csv');
 const { writeFileSync } = require('fs');
 const path = require('path');
+const { dbClient } = require('../db/db.js');
+const { collections } = require('../db/schema.js');
 require('dotenv').config({ path: '../.env' });
 
 const roles = [
@@ -16,6 +15,14 @@ const roles = [
   'Site Administrative Staff',
 ];
 
+const mapToObject = (m) => {
+  const o = {};
+  m.forEach((v, k) => {
+    o[k] = v;
+  });
+  return o;
+};
+
 /* eslint-disable no-console */
 (async () => {
   if (require.main === module) {
@@ -26,55 +33,52 @@ const roles = [
         order: [{
           field: 'created_at',
           direction: 'desc',
-          nulls: 'first'
-        }]
+          nulls: 'first',
+        }],
       });
 
       console.log(`Found ${results.length} record(s), writing CSV file...`);
 
       const csvStream = csv.format({ headers: true });
 
-      results.forEach(result => {
-        let fields = {
-          'Submitted Date': result.created_at,
-          'Registered Business Name': result.body.registeredBusinessName,
-          'Operator Name': result.body.operatorName,
-          'Operator Contact First Name': result.body.operatorContactFirstName,
-          'Operator Contact Last Name': result.body.siteContactLastName,
-          'Operator Contact Email': result.body.operatorEmail,
-          'Operator Contact Phone': result.body.operatorPhone,
-          'Business Operator Address': result.body.operatorAddress,
-          'Operator Postal Code': result.body.operatorPostalCode,
-          'Site Name': result.body.siteName,
-          'Site Address': result.body.address,
-          'Health Authority': result.body.healthAuthority,
-          'Site Contact First Name': result.body.siteContactFirstName,
-          'Site Contact Last Name': result.body.siteContactLastName,
-          'Site Contact Email': result.body.emailAddress,
-          'Site Contact Phone Number': result.body.phoneNumber,
-          'Site Type': result.body.siteType,
-          'Other Site Type': result.body.otherSite,
-          'Number of Public Long Term Care Beds': result.body.numPublicLongTermCare,
-          'Number of Private Long Term Care Beds': result.body.numPrivateLongTermCare,
-          'Number Public Assisted Living Beds': result.body.numPublicAssistedLiving,
-          'Number of Private Assisted Living Beds': result.body.numPrivateAssistedLiving,
-          'Health Care Support Workers for Site': result.body.hcswFteNumber,
-          'Staffing Challenges': result.body.staffingChallenges,
-        };
+      results.forEach((result) => {
+        const columns = new Map();
+        columns.set('Submitted Date', result.created_at);
+        columns.set('Registered Business Name', result.body.registeredBusinessName);
+        columns.set('Operator Name', result.body.operatorName);
+        columns.set('Operator Contact First Name', result.body.operatorContactFirstName);
+        columns.set('Operator Contact Last Name', result.body.siteContactLastName);
+        columns.set('Operator Contact Email', result.body.operatorEmail);
+        columns.set('Operator Contact Phone', result.body.operatorPhone);
+        columns.set('Business Operator Address', result.body.operatorAddress);
+        columns.set('Operator Postal Code', result.body.operatorPostalCode);
+        columns.set('Site Name', result.body.siteName);
+        columns.set('Site Address', result.body.address);
+        columns.set('Health Authority', result.body.healthAuthority);
+        columns.set('Site Contact First Name', result.body.siteContactFirstName);
+        columns.set('Site Contact Last Name', result.body.siteContactLastName);
+        columns.set('Site Contact Email', result.body.emailAddress);
+        columns.set('Site Contact Phone Number', result.body.phoneNumber);
+        columns.set('Site Type', result.body.siteType);
+        columns.set('Other Site Type', result.body.otherSite);
+        columns.set('Number of Public Long Term Care Beds', result.body.numPublicLongTermCare);
+        columns.set('Number of Private Long Term Care Beds', result.body.numPrivateLongTermCare);
+        columns.set('Number Public Assisted Living Beds', result.body.numPublicAssistedLiving);
+        columns.set('Number of Private Assisted Living Beds', result.body.numPrivateAssistedLiving);
+        columns.set('Health Care Support Workers for Site', result.body.hcswFteNumber);
 
-        roles.forEach(role => {
-          fields = {
-            ...fields,
-            [`${role} Current Casual`]: result.body.workforceBaseline.find(item => item.role === role).currentCasual,
-            [`${role} Current Full Time`]: result.body.workforceBaseline.find(item => item.role === role).currentFullTime,
-            [`${role} Vacancy Casual`]: result.body.workforceBaseline.find(item => item.role === role).vacancyCasual,
-            [`${role} Current Part Time`]: result.body.workforceBaseline.find(item => item.role === role).currentPartTime,
-            [`${role} Vacancy Full Time`]: result.body.workforceBaseline.find(item => item.role === role).vacancyFullTime,
-            [`${role} Vacancy Part Time`]: result.body.workforceBaseline.find(item => item.role === role).vacancyPartTime,
-          };
+        roles.forEach((role) => {
+          columns.set(`${role} Current Casual`, result.body.workforceBaseline.find((i) => i.role === role).currentCasual);
+          columns.set(`${role} Current Full Time`, result.body.workforceBaseline.find((i) => i.role === role).currentFullTime);
+          columns.set(`${role} Current Part Time`, result.body.workforceBaseline.find((i) => i.role === role).currentPartTime);
+          columns.set(`${role} Vacancy Casual`, result.body.workforceBaseline.find((i) => i.role === role).vacancyCasual);
+          columns.set(`${role} Vacancy Full Time`, result.body.workforceBaseline.find((i) => i.role === role).vacancyFullTime);
+          columns.set(`${role} Vacancy Part Time`, result.body.workforceBaseline.find((i) => i.role === role).vacancyPartTime);
         });
 
-        csvStream.write(fields);
+        columns.set('Staffing Challenges', result.body.staffingChallenges);
+
+        csvStream.write(mapToObject(columns));
       });
 
       const chunks = [];
@@ -84,7 +88,7 @@ const roles = [
         .on('end', () => {
           const string = Buffer.concat(chunks).toString();
           writeFileSync(path.join(__dirname, 'export.csv'), string);
-          console.log(`Done.`);
+          console.log('Done');
         });
 
       csvStream.end();

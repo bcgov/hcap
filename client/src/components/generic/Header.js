5931b72eb02d3f6a0b9eef81813e7c4bd5dcb170
mauriciofreshw <57013645+mauriciofreshw@users.noreply.github.com>
2020-11-10 14:10:40 -0800

    Logout button + functionality (#96)
    * Logout button
    
    * removed console logs
    
    * removed login comment
    
    Co-authored-by: mauriciofreshw <mauriciojourdan@Mauricios-MBP.hitronhub.home>

--- client/src/components/generic/Header.js
+++ client/src/components/generic/Header.js
@@ -4,10 +4,13 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
+import { useKeycloak } from '@react-keycloak/web';
+import store from 'store';
 import BcLogo from '../../assets/images/bc-logo.svg';
 import BcLogoMini from '../../assets/images/bc-logo-mini.svg';
 import Hidden from '@material-ui/core/Hidden';
 import { Routes } from '../../constants';
+import { Button } from './Button';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,6 +24,7 @@ const useStyles = makeStyles((theme) => ({
   },
   toolbar: {
     height: '100%',
+    justifyContent: 'space-between',
   },
   logoWrapper: {
     display: 'flex',
@@ -45,14 +49,25 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: '#E2A014',
     color: '#E2A014',
     borderStyle: 'solid',
-  }
+  },
+  button: {
+    width: '90px',
+    padding: theme.spacing(1, 3),
+    margin: theme.spacing(0, 1),
+  },
 }));
 
 export const Header = () => {
   const history = useHistory();
   const classes = useStyles();
+  const [keycloak] = useKeycloak();
 
   const handleLogoClick = () => history.push(Routes.EmployerForm);
+
+  const handleLogoutClick = async () => {
+    store.remove('TOKEN');
+    await keycloak.logout({ redirectUri: window.location.origin });
+  };
 
   return (
     <div className={classes.root}>
@@ -80,6 +95,15 @@ export const Header = () => {
               Employer Expression of Interest
             </Typography>
           </div>
+          {(keycloak.authenticated && !keycloak.loginRequired) && (
+            <Button
+              className={classes.button}
+              text="Logout"
+              variant="outlined"
+              color="inherit"
+              onClick={handleLogoutClick}
+            />
+          )}
         </Toolbar>
       </AppBar>
     </div>

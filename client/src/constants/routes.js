cf249ea9e66a3d913fb68015e6d7f20a9917b2f9
mauriciofreshw <mauriciojourdan@Mauricios-MBP.hitronhub.home>
2020-11-09 16:37:30 -0800

    route concerns separated

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -1,10 +1,12 @@
 export default Object.freeze({
   // Public routes
   Login: '/login',
-  Admin: '/admin',
   Keycloak: '/keycloak',
   EmployerForm: '/',
   EmployeeForm: '/employee-form',
   EmployeeConfirmation: '/employee-confirmation',
   EmployerConfirmation: '/employer-confirmation',
+
+  // Private routes
+  Admin: '/admin',
 });

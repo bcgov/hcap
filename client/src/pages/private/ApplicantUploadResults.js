4d9ea99bbe54c6f6780c54ccb1e5994dedbd561c
Ashton Meuser <ashtonmeuser@users.noreply.github.com>
2020-11-18 18:33:18 -0800

    Added success message to applicant upload results (#112)
    * Added success message to applicant upload results
    
    * More detailed summary on applicant upload

--- client/src/pages/private/ApplicantUploadResults.js
+++ client/src/pages/private/ApplicantUploadResults.js
@@ -3,6 +3,7 @@ import _orderBy from 'lodash/orderBy';
 import { useHistory, useLocation, Redirect } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import { Box, Typography } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
 import { Button, Page, Table, CheckPermissions } from '../../components/generic';
 import { Routes } from '../../constants';
 import store from 'store';
@@ -12,6 +13,7 @@ export default () => {
   const [roles, setRoles] = useState([]);
   const [order, setOrder] = useState('asc');
   const [rows, setRows] = useState([]);
+  const [summary, setSummary] = useState({ duplicates: 0, errors: 0 });
   const [columns] = useState([
     { id: 'id', name: 'ID' },
     { id: 'status', name: 'Status' },
@@ -52,6 +54,10 @@ export default () => {
   useEffect(() => {
     fetchUserInfo();
     const insertMissingMessage = (row) => row.message ? row : {...row, message: ''};
+    setSummary({
+      duplicates: location.state?.results?.filter((x) => x.status === 'Duplicate').length || 0,
+      errors: location.state?.results?.filter((x) => x.status === 'Error').length || 0,
+    });
     setRows(location.state?.results.map(insertMissingMessage) || []);
   }, [location]);
 
@@ -64,7 +70,14 @@ export default () => {
               Applicant Upload Results
             </Typography>
           </Box>
-          <Box pb={4} pl={2} pr={2}>
+          <Alert severity={summary.errors ? 'error' : summary.duplicates ? 'warning' : 'success'}>
+            <Typography variant="body2" gutterBottom>
+              <b>
+                Processed {rows.length} applicants. There were {summary.errors} errors and {summary.duplicates} duplicates. See results below.
+              </b>
+            </Typography>
+          </Alert>
+          <Box pt={4} pb={4} pl={2} pr={2}>
             <Button
                 onClick={() => history.push(Routes.ApplicantUpload)}
                 text="Return to Applicant Upload"

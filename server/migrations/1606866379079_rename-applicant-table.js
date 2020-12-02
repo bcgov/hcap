678fcf56aae8f4418da64e5d6309dcff5ac87ff0
mauriciofreshw <mauriciojourdan@Mauricios-MBP.hitronhub.home>
2020-12-01 16:16:21 -0800

    fixed migrations renametable

--- server/migrations/1606866379079_rename-applicant-table.js
+++ server/migrations/1606866379079_rename-applicant-table.js
@@ -1,5 +1,8 @@
 /* eslint-disable camelcase */
 
-exports.up = (pgm) => {
-  pgm.renameTable('applicants', 'participants');
+exports.up = async (pgm) => {
+  const applicantsExists = await pgm.db.query('SELECT \'public.applicants\'::regclass');
+  if (applicantsExists) {
+    pgm.renameTable('applicants', 'participants');
+  }
 };

d41164bc412d2b12ad6c9b1e3c51856464586843
Ashton Meuser <ashtonmeuser@users.noreply.github.com>
2020-12-14 17:44:02 -0800

    Agreement notice and styling of pending access (#166)

--- client/src/components/generic/CheckPermissions.js
+++ client/src/components/generic/CheckPermissions.js
@@ -1,39 +1,41 @@
 import React, { Fragment } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Box, Typography } from '@material-ui/core';
+import { Card } from '.';
 import { checkPermissions, checkPending } from '../../utils';
 
 export const CheckPermissions = ({ isLoading, roles, permittedRoles, children, renderErrorMessage = false }) => {
   const isPending = checkPending(roles);
-  if (isLoading) {
-    return null;
-  }
+
+  // If page is currently loading, don't render anything
+  if (isLoading) return null;
 
   // If user has a permitted role assigned, grant access to components
-  if (checkPermissions(roles, permittedRoles)) {
-    return children;
-  }
+  if (checkPermissions(roles, permittedRoles)) return children;
 
-  const pendingMessage = <Fragment>
+  // Default: return nothing if no access granted by role(s)
+  if (!renderErrorMessage) return null;
+
+  const permissionPendingMessage = <Fragment>
     <p>Thank you for logging in. Your access request is being reviewed.</p>
     <p>Please note that your access will only be granted if you have approved allocations under HCAP.</p>
+    <p>Information in the EEOI portal has been collected under sections 26(c) and (e) of the Freedom of Information and Protection of Privacy Act (FOIPPA) for the purposes of administering the Health Career Access Program. Information will only be used by authorized personnel to fulfill the purpose for which it was originally collected or for a use consistent with that purpose. Information to other public bodies or individuals will not be disclosed except as authorized by FOIPPA.</p>
   </Fragment>;
 
-  const message = isPending ? pendingMessage : <p>You don't have permission to view this content.</p>;
+  const permissionDeniedMessage = <p>You don't have permission to view this content.</p>;
 
-  // Optional: display message if no access granted by role(s)
-  if (renderErrorMessage) {
-    return (
-      <Grid container alignContent="center" justify="center" alignItems="center" direction="column">
-        <Box pb={4} pl={4} pr={4} pt={2}>
+  const message = isPending ? permissionPendingMessage : permissionDeniedMessage;
+
+  // Optional message displayed if access not granted by role(s)
+  return (
+    <Grid container alignContent="center" justify="center" alignItems="center" direction="column">
+      <Box pb={4} pl={4} pr={4} pt={2} maxWidth={800}>
+        <Card>
           <Typography variant="subtitle1" gutterBottom>
             { message }
           </Typography>
-        </Box>
-      </Grid>
-    );
-  }
-
-  // Default: return nothing if no access granted by role(s)
-  return null;
+        </Card>
+      </Box>
+    </Grid>
+  );
 };

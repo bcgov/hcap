ddbe3ad491b7a2e3fa0b26adb06abb2c892e7a83
zenen-freshworks <zenen@freshworks.io>
2020-12-23 15:43:39 -0800

    Added better page navigation

--- client/src/components/generic/Table.js
+++ client/src/components/generic/Table.js
@@ -4,10 +4,16 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
+import { List, ListItem, ListItemText, Menu, MenuItem } from '@material-ui/core';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TablePagination from '@material-ui/core/TablePagination';
 import Skeleton from '@material-ui/lab/Skeleton';
-import { withStyles } from '@material-ui/core/styles';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import IconButton from '@material-ui/core/IconButton';
+import FirstPageIcon from '@material-ui/icons/FirstPage';
+import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
+import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
+import LastPageIcon from '@material-ui/icons/LastPage';
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -46,6 +52,111 @@ const StyledTableRow = withStyles((theme) => ({
     },
   },
 }))(TableRow);
+
+const useStyles1 = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexShrink: 0,
+    marginLeft: theme.spacing(2.5),
+  },
+}));
+
+const TablePaginationActions = (props) => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const classes = useStyles1();
+  const { count, page, rowsPerPage, onChangePage } = props;
+  const pages = [...Array(Math.ceil(count / rowsPerPage)).keys()];
+
+  const handleFirstPageButtonClick = (event) => {
+    onChangePage(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onChangePage(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onChangePage(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  const handleClickListItem = (event) => {
+    setAnchorEl(event.currentTarget);
+  }
+
+  const handleMenuItemClick = (event, index) => {
+    onChangePage(event, index);
+    setAnchorEl(null);
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  }
+
+
+  return (
+    <div className={classes.root}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        <FirstPageIcon />
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        <KeyboardArrowLeft />
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        <KeyboardArrowRight />
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        <LastPageIcon />
+      </IconButton>
+    <List component="nav" aria-label="Page Selector">
+      <ListItem
+        button
+        aria-haspopup="true"
+        aria-controls="page-select"
+        aria-label="go to page"
+        onClick={handleClickListItem}
+      >
+        <ListItemText primary="Skip to page..." />
+      </ListItem>
+    </List>
+    <Menu
+      id="page-select"
+      anchorEl={anchorEl}
+      keepMounted
+      open={Boolean(anchorEl)}
+      onClose={handleClose}
+    >
+      {pages.map((option) => (
+        <MenuItem
+          key={`page ${option}`}
+          selected={option === page}
+          onClick={(event) => handleMenuItemClick(event, option)}
+        >
+          Page {option+1}
+        </MenuItem>
+      ))}
+    </Menu>
+  </div>
+  )
+};
+
+
 
 export const Table = ({ order, orderBy, renderCell, onRequestSort, columns, rows, isLoading, rowsPerPage = 10 }) => {
 
@@ -114,6 +225,7 @@ export const Table = ({ order, orderBy, renderCell, onRequestSort, columns, rows
         rowsPerPage={rowsPerPage}
         page={page}
         onChangePage={onChangePage}
+        ActionsComponent={TablePaginationActions}
       />
     </Fragment>
   );

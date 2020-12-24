5b2547c01c9dccae9953d0b2d8d86661f173502f
zenen-freshworks <zenen@freshworks.io>
2020-12-24 10:39:40 -0800

    future date no longer selectable

--- client/src/components/modal-forms/InterviewingForm.js
+++ client/src/components/modal-forms/InterviewingForm.js
@@ -17,6 +17,7 @@ export const InterviewingForm = ({ initialValues, validationSchema, onSubmit, on
           <Field
             name="contactedDate"
             component={RenderDateField}
+            maxDate={new Date().toISOString().split('T')[0]}
             label="* Contacted Date"
           />
         </Box>

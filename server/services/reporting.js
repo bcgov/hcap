145b4b97676812c16e1ffbc434118c1d972c1017
mauriciofreshw <mauriciojourdan@Galaxy-A30.hitronhub.home>
2021-01-07 14:09:16 -0800

    ignore hireds on in progress

--- server/services/reporting.js
+++ server/services/reporting.js
@@ -6,9 +6,30 @@ const getReport = async () => {
     interested: 'yes',
     crcClear: 'yes',
   });
+
+  /// ///////////
+  const inProgress1 = await dbClient.db[collections.PARTICIPANTS_STATUS].join({
+    hiredJoin: {
+      type: 'LEFT OUTER',
+      relation: collections.PARTICIPANTS_STATUS,
+      on: {
+        participant_id: 'participant_id',
+        status: 'hired',
+        current: true,
+      },
+    },
+  }).find({
+    current: true,
+    status: ['prospecting', 'interviewing', 'offer_made'],
+    'hiredJoin.status': null,
+  });
+
+  console.log(inProgress1);
+  /// ///////////
+
   const inProgress = await dbClient.db[collections.PARTICIPANTS_STATUS].count({
     current: true,
-    status: ['prospecting', 'interviewing', 'offer made'],
+    status: ['prospecting', 'interviewing', 'offer_made'],
   }, {
     fields: ['participant_id'],
     distinct: true,

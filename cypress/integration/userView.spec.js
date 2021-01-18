03abd2f7f706ba019ff2bffac82dea37fc0cfd85
zenen-freshworks <zenen@freshworks.io>
2021-01-18 11:14:56 -0800

    added some documentation

--- cypress/integration/userView.spec.js
+++ cypress/integration/userView.spec.js
@@ -4,10 +4,20 @@ describe("Tests the User View", () => {
   })
 
   it("tests failed access request approval", () => {
+
+    // kcNavAs is the command that bypasses keycloak login. First parameter
+    // indicates the roles to be implemented upon navigating to the url in the
+    // second parameter
     cy.kcNavAs("MoH", "user-view");
     cy.contains('Options').click();
+
+    // Cypress locates objects in the DOM via CSS selector syntax. Afterwards,
+    // the component can be tested or interacted with.
     cy.get('div#mui-component-select-role').click();
     cy.get('li').contains("Ministry").click();
+
+    // Should statements form the basis for cypress tests, check the
+    // documentation for details on how to use them
     cy.get('input[name=acknowledgement]').should('have.attr', 'value', 'false');
     cy.get('button').contains('Submit').click();
     cy.get('p.Mui-error').should('be.visible');

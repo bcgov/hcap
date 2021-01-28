d091a439a714742903bf3ee98c6d62eaee467669
zenen-freshworks <zenen@freshworks.io>
2021-01-28 11:06:34 -0800

    Added form

---
+++ client/src/components/modal-forms/EditParticipantForm.js
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from 'react';
+import Grid from '@material-ui/core/Grid';
+import { Button } from '../generic';
+import { Box } from '@material-ui/core';
+import { RenderDateField, RenderCheckbox, RenderTextField, RenderSelectField } from '../fields';
+import { Field, Formik, Form as FormikForm } from 'formik';
+import store from 'store';
+
+export const EditParticipantForm = ({ initialValues, validationSchema, onSubmit, onClose, sites }) => {
+
+  const [sitesDetail, setSitesDetail] = useState([]);
+
+  useEffect(() => {
+    const fetchSites = async () => {
+      const response = await fetch('/api/v1/employer-sites', {
+        headers: { 'Authorization': `Bearer ${store.get('TOKEN')}` },
+        method: 'GET',
+      });
+
+      if (response.ok) {
+        const { data } = await response.json();
+        setSitesDetail(data.filter((site) => sites.includes(site.siteId)));
+      }
+    };
+
+    fetchSites();
+  }, [sites]);
+
+  const getTodayDate = () => {
+    const today = new Date();
+    const dd = String(today.getDate()).padStart(2, '0');
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const yyyy = today.getFullYear();
+    return `${yyyy}-${mm}-${dd}`;
+  }
+
+  return <Formik
+    initialValues={initialValues}
+    validationSchema={validationSchema}
+    onSubmit={onSubmit}
+  >
+    {({ submitForm, values }) => (
+      <FormikForm>
+        <Box>
+      <Field
+      name="firstName"
+      component={RenderTextField}
+      label="First Name"
+      />
+      <Field
+      name="lastName"
+      component={RenderTextField}
+      label="Last Name"
+      />
+      <Field
+      name="phoneNumber"
+      component={RenderTextField}
+      label="* Phone Number"
+      type="tel"
+      />
+      <Field
+      name="emailAddress"
+      component={RenderTextField}
+      label="* Email Address"
+      type="email"
+      />
+          <Field
+            name="nonHcapOpportunity"
+            component={RenderCheckbox}
+            label="Non-HCAP Opportunity"
+          />
+          {
+            values.nonHcapOpportunity && (<>
+              <Field
+                name="positionType"
+                component={RenderSelectField}
+                label="* Position Type"
+                options={[
+                  { value: 'Full-Time', label: 'Full-Time' },
+                  { value: 'Part-Time', label: 'Part-Time' },
+                  { value: 'Casual', label: 'Casual' },
+                ]}
+              />
+            </>)
+          }
+          <Field
+            name="hiredDate"
+            component={RenderDateField}
+            maxDate={getTodayDate()}
+            label="* Date Hired"
+          />
+          <Field
+            name="startDate"
+            component={RenderDateField}
+            label="* Start Date"
+          />
+          <Field
+            name="site"
+            component={RenderSelectField}
+            label="* Site"
+            options={sitesDetail.map((siteDetail) => ({
+              value: siteDetail.siteId, label: siteDetail.siteName,
+            }))}
+          />
+          <Field
+            name="acknowledge"
+            component={RenderCheckbox}
+            label="I acknowledge that the participant has accepted the offer in writing."
+          />
+        </Box>
+        <Box mt={3}>
+          <Grid container spacing={2} justify="flex-end">
+            <Grid item>
+              <Button
+                onClick={onClose}
+                color="default"
+                text="Cancel"
+              />
+            </Grid>
+            <Grid item>
+              <Button
+                onClick={submitForm}
+                variant="contained"
+                color="primary"
+                text="Submit"
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      </FormikForm>
+    )}
+  </Formik>;
+};

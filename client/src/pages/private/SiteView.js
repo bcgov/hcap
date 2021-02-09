e21b4abeec252d87d3d08b7fa993420cb1d9c4aa
zenen-freshworks <zenen@freshworks.io>
2021-02-09 10:26:40 -0800

    WIP

--- client/src/pages/private/SiteView.js
+++ client/src/pages/private/SiteView.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, lazy } from 'react';
 import { Page, CheckPermissions } from '../../components/generic';
+import { Box, Typography } from '@material-ui/core';
 import store from 'store';
 
 const SiteTable = lazy(() => import('./SiteTable'));
@@ -31,6 +32,11 @@ export default () => {
 
   return (
     <Page>
+      <Box pt={4} pb={4} pl={2} pr={2}>
+        <Typography variant="subtitle1" gutterBottom>
+          Sites
+        </Typography>
+      </Box>
       <CheckPermissions 
         isLoading={isLoadingUser} 
         roles={roles} 

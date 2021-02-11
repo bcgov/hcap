b1f189b4ad0a561ae9e632c5d73357e19f017eab
Mauricio Jourdan <mauriciomirandajourdan@gmail.com>
2021-02-11 14:39:12 -0800

    added site view layout

--- client/src/pages/private/SiteViewDetails.js
+++ client/src/pages/private/SiteViewDetails.js
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Page, CheckPermissions } from '../../components/generic';
+import { Box, Typography } from '@material-ui/core';
 import { scrollUp } from '../../utils';
 import store from 'store';
+import { Link } from 'react-router-dom';
+import routes from '../../constants/routes';
+
 
 export default (props) => {
   const [roles, setRoles] = useState([]);
@@ -47,13 +51,77 @@ export default (props) => {
     fetchDetails();
   }, [siteID]);
 
+  const fieldsLabelMap = {
+    'Site Contact': {
+      'First Name': 'siteContactFirstName',
+      'Last Name': 'siteContactLastName',
+    },
+    'Site Info': {
+      'Site Name': 'siteName',
+      'Business Name': 'registeredBusinessName',
+      'Address': 'address',
+      'Postal Code': 'postalCode',
+      'City': 'city',
+      'Phase 1 Allocation': 'earlyAdopterAllocation',
+      'Region': 'healthAuthority',
+    },
+    'Operator Info': {
+      'First Name': 'operatorContactFirstName',
+      'Last Name': 'operatorContactLastName',
+      'Phone Number': 'operatorPhone',
+      'Email address': 'operatorEmail',
+    },
+  };
+
   scrollUp();
   return (
     <Page>
       <CheckPermissions isLoading={isLoadingUser} roles={roles} permittedRoles={['health_authority', 'ministry_of_health']} renderErrorMessage={true}>
-        <Grid item xs={12} sm={11} md={10} lg={8} xl={6}>
-          <p>{JSON.stringify(site)}</p>
-        </Grid>
+        <Box pt={4} pb={2} pl={4} pr={4}>
+          <Box pb={4}>
+            <Box pb={2}>
+              <Typography variant="body1">
+                <Link to={routes.ParticipantView}>Sites</Link> / {site.siteName}
+              </Typography>
+            </Box>
+            <Typography variant="h2">
+              <b>Site Details</b>
+            </Typography>
+          </Box>
+          <Grid container>
+            {
+              Object.keys(fieldsLabelMap).map(title =>
+                <Grid key={title} item>
+                  <Box pr={12}>
+                    <Box pb={2}>
+                      <Typography variant="subtitle1">
+                        <b>{title}</b>
+                      </Typography>
+                    </Box>
+                    {
+                      Object.keys(fieldsLabelMap[title]).map(subTitle =>
+                        <Grid key={subTitle}
+                          justify="space-between"
+                          container>
+                          <Grid item>
+                            <Box pr={4} pb={1}>
+                              <Typography variant="body1">
+                                <b>{subTitle}</b>
+                              </Typography>
+                            </Box>
+                          </Grid>
+                          <Grid item>
+                            <Typography variant="body1">
+                              {site[fieldsLabelMap[title][subTitle]]}
+                            </Typography>
+                          </Grid>
+                        </Grid>)
+                    }
+                  </Box>
+                </Grid>)
+            }
+          </Grid>
+        </Box>
       </CheckPermissions>
     </Page>
   );

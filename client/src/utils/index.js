ef74d0a0b96257ccd003b21ae22bf456b2050075
zenen-freshworks <zenen@freshworks.io>
2021-02-26 15:04:50 -0800

    using moment instead of fuzzydateoffset

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -3,7 +3,6 @@ export * from './pdf';
 export * from './object';
 export * from './misc';
 export * from './permissions';
-export * from './fuzzyDateOffset';
 export * from './getDialogTitle';
 export * from './prettifyStatus';
 

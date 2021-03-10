807bd8125e41cd01ff0d52f44efa442e068a5d31
Mauricio Jourdan <mauricio@freshworks.io>
2021-03-10 09:50:20 -0800

    added indexes

---
+++ server/migrations/1615361765942_index-optimization.js
@@ -0,0 +1,10 @@
+const { collections } = require('../db/schema.js');
+
+exports.up = async (pgm) => {
+  await pgm.createIndex(collections.PARTICIPANTS_STATUS, 'status');
+  await pgm.createIndex(collections.PARTICIPANTS_STATUS, 'employer_id');
+  await pgm.createIndex(collections.PARTICIPANTS_STATUS, 'participant_id');
+  await pgm.createIndex(collections.PARTICIPANTS_STATUS, 'current', { where: 'current = true' });
+  await pgm.createExtension('pg_trgm');
+  await pgm.createIndex(collections.PARTICIPANTS, [{ name: "(body->>'preferredLocation')", opclass: { name: 'gin_trgm_ops' } }], { method: 'gin' });
+};

85681eded74eac3eb7d4fa5069e1087fc977b315
Mauricio Jourdan <mauricio@freshworks.io>
2021-03-10 10:21:28 -0800

    added ifnotexist

--- server/migrations/1615361765942_index-optimization.js
+++ server/migrations/1615361765942_index-optimization.js
@@ -5,6 +5,6 @@ exports.up = async (pgm) => {
   await pgm.createIndex(collections.PARTICIPANTS_STATUS, 'employer_id');
   await pgm.createIndex(collections.PARTICIPANTS_STATUS, 'participant_id');
   await pgm.createIndex(collections.PARTICIPANTS_STATUS, 'current', { where: 'current = true' });
-  await pgm.createExtension('pg_trgm');
+  await pgm.createExtension('pg_trgm', { ifNotExists: true });
   await pgm.createIndex(collections.PARTICIPANTS, [{ name: "(body->>'preferredLocation')", opclass: { name: 'gin_trgm_ops' } }], { method: 'gin' });
 };

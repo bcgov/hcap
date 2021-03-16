9183d23632fe57b2659290615c07e442efc792e6
mauriciofreshw <57013645+mauriciofreshw@users.noreply.github.com>
2021-03-16 11:40:13 -0700

    Accessibility fixes (#316)
    * accessbility improvemnets
    
    * pr fix

--- client/src/components/fields/RenderTextField.js
+++ client/src/components/fields/RenderTextField.js
@@ -5,13 +5,13 @@ import { ErrorMessage } from 'formik';
 import { InputFieldLabel, InputFieldError } from '../generic';
 
 export const RenderTextField = ({
-  field: { value, ...fieldRest },
+  field: { value, name, ...fieldRest },
   form,
   label,
   ...props
 }) => {
-  const touched = form.touched[fieldRest.name];
-  const error = form.errors[fieldRest.name];
+  const touched = form.touched[name];
+  const error = form.errors[name];
 
   const sanitizeValue = (value) => {
     if (value === null) return '';
@@ -20,16 +20,23 @@ export const RenderTextField = ({
 
   return (
     <Fragment>
-      {label && <InputFieldLabel label={label} />}
+      <label htmlFor={name}>
+        {label && <InputFieldLabel label={label} />}
+      </label>
       <TextField
+        id={name}
+        name={name}
         variant="filled"
         fullWidth
         error={touched && !!error}
         value={sanitizeValue(value)}
+        aria-required
+        aria-invalid={!!error}
+        aria-describedby={`${name}Error`}
         {...fieldRest}
         {...props}
       />
-      <InputFieldError error={<ErrorMessage name={fieldRest.name} />} />
+      <InputFieldError id={`${name}Error`} error={<ErrorMessage name={name} />} />
     </Fragment>
   );
 };

51322d1f1b0de67ab4122b00dac0b7f31613671a
Mauricio Jourdan <mauricio@freshworks.io>
2021-03-15 17:41:08 -0700

    pr fix

--- client/src/components/fields/RenderTextField.js
+++ client/src/components/fields/RenderTextField.js
@@ -5,13 +5,13 @@ import { ErrorMessage } from 'formik';
 import { InputFieldLabel, InputFieldError } from '../generic';
 
 export const RenderTextField = ({
-  field: { value, ...fieldRest },
+  field: { value, name, ...fieldRest },
   form,
   label,
   ...props
 }) => {
-  const touched = form.touched[fieldRest.name];
-  const error = form.errors[fieldRest.name];
+  const touched = form.touched[name];
+  const error = form.errors[name];
 
   const sanitizeValue = (value) => {
     if (value === null) return '';
@@ -20,23 +20,23 @@ export const RenderTextField = ({
 
   return (
     <Fragment>
-      <label htmlFor={fieldRest.name}>
+      <label htmlFor={name}>
         {label && <InputFieldLabel label={label} />}
       </label>
       <TextField
-        id={fieldRest.name}
-        name={fieldRest.name}
+        id={name}
+        name={name}
         variant="filled"
         fullWidth
         error={touched && !!error}
         value={sanitizeValue(value)}
         aria-required
         aria-invalid={!!error}
-        aria-describedby={`${fieldRest.name}Error`}
+        aria-describedby={`${name}Error`}
         {...fieldRest}
         {...props}
       />
-      <InputFieldError id={`${fieldRest.name}Error`} error={<ErrorMessage name={fieldRest.name} />} />
+      <InputFieldError id={`${name}Error`} error={<ErrorMessage name={name} />} />
     </Fragment>
   );
 };

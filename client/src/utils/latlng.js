19146c8a6e4f9543654ad62613e7cf1745871a67
zenen-freshworks <zenen@freshworks.io>
2021-03-17 13:00:13 -0700

    added util

---
+++ client/src/utils/latlng.js
@@ -0,0 +1,18 @@
+export const pointsToDistance = (json) => {
+    const { p1: { lat1, lng1 }, p2: { lat2, lng2 } } = json;
+    
+    const R = 6371e3; // metres
+    const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+    const φ2 = lat2 * Math.PI/180;
+    const Δφ = (lat2-lat1) * Math.PI/180;
+    const Δλ = (lng2-lng1) * Math.PI/180;
+
+    const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+        Math.cos(φ1) * Math.cos(φ2) *
+        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+    const d = R * c; // in metres
+
+    return d;
+}

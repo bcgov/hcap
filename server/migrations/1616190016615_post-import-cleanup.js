a98f7b86e2823bd6377222c842ff335de16145ee
Mauricio Jourdan <mauricio@freshworks.io>
2021-03-19 16:53:05 -0700

    added post import migration

---
+++ server/migrations/1616190016615_post-import-cleanup.js
@@ -0,0 +1,27 @@
+/* eslint-disable camelcase */
+const { dbClient, collections } = require('../db');
+
+exports.up = async () => {
+  await dbClient.db.withTransaction(async (tx) => {
+    await tx[collections.PARTICIPANTS].updateDoc(
+      {
+        postalCode: 'Z1Z1Z1',
+      },
+      { postalCode: '' },
+    );
+
+    await tx[collections.PARTICIPANTS].updateDoc(
+      {
+        nonHCAP: 'NULL',
+      },
+      { nonHCAP: '' },
+    );
+
+    await tx[collections.PARTICIPANTS].updateDoc(
+      {
+        crcClear: 'NULL',
+      },
+      { crcClear: '' },
+    );
+  });
+};

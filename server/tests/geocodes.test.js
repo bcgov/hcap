20312f8323187898c04dfc617cd2c4c24e2e5857
zenen-freshworks <zenen@freshworks.io>
2021-04-29 15:55:10 -0700

    commented out test

--- server/tests/geocodes.test.js
+++ server/tests/geocodes.test.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-restricted-syntax, no-await-in-loop */
-const { v4 } = require('uuid');
+// const { v4 } = require('uuid');
 const { startDB, closeDB } = require('./util/db');
-const { getUserSites, makeUser } = require('../services/user.js');
-const { saveSites } = require('../services/employers.js');
-const { makeParticipant, getParticipants } = require('../services/participants.js');
-const { getParticipantCoords } = require('../services/geocodes.js');
+// const { getUserSites, makeUser } = require('../services/user.js');
+// const { saveSites } = require('../services/employers.js');
+// const { makeParticipant, getParticipants } = require('../services/participants.js');
+// const { getParticipantCoords } = require('../services/geocodes.js');
 
 describe('Geocodes Service', () => {
   beforeAll(async () => {
@@ -21,26 +21,26 @@ describe('Geocodes Service', () => {
   // Add site, query for coords and compare
   // Update site postal codes, query for new coords
 
-  it('Adds and updates a participant postal code, checking for coords', async () => {
-    const participant = {
-      maximusId: 648690,
-      lastName: 'Extra',
-      firstName: 'Eddy',
-      postalCode: 'V0A2V3',
-      postalCodeFsa: 'V1V',
-      phoneNumber: '2502223333',
-      emailAddress: 'eddy@example.com',
-      interested: 'yes',
-      nonHCAP: 'yes',
-      crcClear: 'yes',
-      preferredLocation: 'Fraser',
-    };
+  // it('Adds and updates a participant postal code, checking for coords', async () => {
+  //   const participant = {
+  //     maximusId: 648690,
+  //     lastName: 'Extra',
+  //     firstName: 'Eddy',
+  //     postalCode: 'V0A2V3',
+  //     postalCodeFsa: 'V1V',
+  //     phoneNumber: '2502223333',
+  //     emailAddress: 'eddy@example.com',
+  //     interested: 'yes',
+  //     nonHCAP: 'yes',
+  //     crcClear: 'yes',
+  //     preferredLocation: 'Fraser',
+  //   };
 
-    const res = await makeParticipant(participant);
-    const coords = await getParticipantCoords(res.id);
+  //   const res = await makeParticipant(participant);
+  //   const coords = await getParticipantCoords(res.id);
 
-    console.log(coords);
-  });
+  //   console.log(coords);
+  // });
 
   // it('Adds a site and updates the postal code, checking for coords', async () => {
   //   const id = v4();

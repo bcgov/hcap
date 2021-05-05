b79f03d34933c5c6142f3a1125a830f29f2a883b
Arran <arran@freshworks.io>
2021-05-05 17:18:29 -0600

    manual edit

--- server/error-handler.js
+++ server/error-handler.js
@@ -1,7 +1,7 @@
 const logger = require('./logger.js');
 
 // Middleware to log and sanitize errors
-const errorHandler = (error, req, res, next) => { // eslint-disable-line no-unused-vars
+const errorHandler = (error, _, res) => {
   logger.error(error.message);
   switch (error.name) {
     case 'ValidationError':
@@ -14,10 +14,7 @@ const errorHandler = (error, req, res, next) => { // eslint-disable-line no-unus
 };
 
 // Wraps async request handlers to ensure next is called
-const asyncMiddleware = (f) => (
-  (req, res, next) => Promise
-    .resolve(f(req, res, next))
-    .catch((error) => next(error))
-);
+const asyncMiddleware = (f) => (req, res, next) =>
+  Promise.resolve(f(req, res, next)).catch((error) => next(error));
 
 module.exports = { errorHandler, asyncMiddleware };

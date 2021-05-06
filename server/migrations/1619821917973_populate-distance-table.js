b5f52aa67c97de9601f7c8e482e8f3e22d924276
Ashton Meuser <ashtonmeuser@users.noreply.github.com>
2021-05-06 15:51:35 -0700

    Composite unique index on distance table (#361)
    * Composite unique index on distance table
    
    * First wipe distance table and add unique index
    
    * Unique index
    
    * NPM audit fix
    
    * Fix server NPM audit
    
    * Update deps
    
    * Ignore dev deps when auditing server
    
    * Create index if not exists

--- server/migrations/1619821917973_populate-distance-table.js
+++ server/migrations/1619821917973_populate-distance-table.js
@@ -3,6 +3,10 @@ const { dbClient, collections } = require('../db');
 exports.shorthands = undefined;
 
 exports.up = async (pgm) => {
+  pgm.sql(`DELETE FROM ${collections.PARTICIPANTS_DISTANCE};`);
+
+  pgm.sql(`CREATE UNIQUE INDEX IF NOT EXISTS unique_site_participant ON ${collections.PARTICIPANTS_DISTANCE} (participant_id, site_id);`);
+
   const participants = await dbClient.db[collections.PARTICIPANTS].findDoc({
     'location IS NOT': null,
   });
@@ -11,14 +15,14 @@ exports.up = async (pgm) => {
   });
 
   const aWholeLottaPromises = sites.flatMap((site) => participants.map((participant) => pgm.sql(`
-    INSERT INTO participants_distance (participant_id, site_id, distance) VALUES (
+    INSERT INTO ${collections.PARTICIPANTS_DISTANCE} (participant_id, site_id, distance) VALUES (
       ${participant.id},
       ${site.siteId},
       ST_DistanceSphere(
         ST_GeomFromGeoJSON('${JSON.stringify(site.location)}'),
         ST_GeomFromGeoJSON('${JSON.stringify(participant.location)}')
       )
-    );`)));
+    ) ON CONFLICT DO NOTHING;`)));
 
   await Promise.allSettled(aWholeLottaPromises);
 };

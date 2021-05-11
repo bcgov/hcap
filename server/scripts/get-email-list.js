e4b6558f327687f50d62f9fb874ea18f5841d782
David Bayly <davidbayly@freshworks.io>
2021-05-11 16:45:20 -0700

    added hired restriction

--- server/scripts/get-email-list.js
+++ server/scripts/get-email-list.js
@@ -3,10 +3,12 @@ require('dotenv').config({ path: '../.env' });
 (async function exec(){
     await dbClient.connect();
     const res = await dbClient.db.query(
-   `Select body->> 'emailAddress' as email 
-    FROM participants 
-    WHERE CAST(body ->> 'interested' AS TEXT) = 'yes' AND
-    created_at < CURRENT_TIMESTAMP - interval '6 weeks'`)
+   `Select DISTINCT body->> 'emailAddress' as email  FROM 
+   participants p 
+   JOIN participants_status ps 
+   on  p.id = ps.participant_id AND ps.status != 'hired'
+   WHERE CAST(body ->> 'interested' AS TEXT) = 'yes'  
+   AND p.created_at < CURRENT_TIMESTAMP - interval '6 weeks'`)
     console.log(res);
     process.exit(0);
 })()

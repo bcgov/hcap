ef68a17ebc22e14ade29fc48cbf524d757e24a42
David Bayly <davidbayly@freshworks.io>
2021-05-11 16:49:04 -0700

    formatting

--- server/scripts/get-email-list.js
+++ server/scripts/get-email-list.js
@@ -3,12 +3,14 @@ require('dotenv').config({ path: '../.env' });
 (async function exec(){
     await dbClient.connect();
     const res = await dbClient.db.query(
-   `Select DISTINCT body->> 'emailAddress' as email  FROM 
-   participants p 
-   JOIN participants_status ps 
-   on  p.id = ps.participant_id AND ps.status != 'hired'
-   WHERE CAST(body ->> 'interested' AS TEXT) = 'yes'  
-   AND p.created_at < CURRENT_TIMESTAMP - interval '6 weeks'`)
+   `SELECT DISTINCT body->> 'emailAddress' AS email 
+    FROM participants p 
+    JOIN participants_status ps 
+        ON  p.id = ps.participant_id AND ps.status != 'hired'
+    WHERE 
+        CAST(p.body ->> 'interested' AS TEXT) = 'yes'  
+        OR CAST(p.body->> 'interested' AS TEXT) IS NULL
+        AND p.created_at < CURRENT_TIMESTAMP - interval '6 weeks'`)
     console.log(res);
     process.exit(0);
 })()

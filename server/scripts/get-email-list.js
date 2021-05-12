15a0f08f44d37ea16d3fffa737bb58c8d68f1ccb
David Bayly <davidbayly@freshworks.io>
2021-05-12 11:17:50 -0700

    Fixed time check and exclude hired entries

--- server/scripts/get-email-list.js
+++ server/scripts/get-email-list.js
@@ -1,18 +1,26 @@
 /* eslint-disable no-console */
 const { dbClient } = require('../db');
 require('dotenv').config({ path: '../.env' });
-(async function exec(){
-    await dbClient.connect();
-    const res = await dbClient.db.query(
-   `SELECT DISTINCT body->> 'emailAddress' AS email 
+(async function exec() {
+  await dbClient.connect();
+  const res = await dbClient.db.query(`
+    SELECT p.body ->> 'emailAddress' AS email
     FROM participants p 
-    JOIN participants_status ps 
-        ON  p.id = ps.participant_id AND ps.status != 'hired'
     WHERE 
-        CAST(p.body ->> 'interested' AS TEXT) = 'yes'  
-        OR CAST(p.body->> 'interested' AS TEXT) IS NULL
-        AND p.created_at < CURRENT_TIMESTAMP - interval '6 weeks'`)
-    console.log(res);
-    process.exit(0);
-})()
-
+        p.body ->> 'emailAddress' NOT IN (
+            SELECT p2.body->> 'emailAddress'
+            FROM participants p2 
+            JOIN participants_status ps
+            ON p2.id = ps.participant_id AND ps.status = 'hired'
+        )
+        AND (CAST(p.body ->> 'interested' AS TEXT) = 'yes'  
+            OR CAST(p.body->> 'interested' AS TEXT) IS NULL)
+        AND (
+                (p.updated_at < CURRENT_TIMESTAMP - interval '6 weeks'
+                AND p.updated_at IS NOT NULL)
+                OR (p.updated_at IS NULL 
+                    AND p.created_at < CURRENT_TIMESTAMP - interval '6 weeks')
+	     )`);
+  console.log(res);
+  process.exit(0);
+})();

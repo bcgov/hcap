6d7eac67db63c0cefdfc3090335aa996d931667b
dbayly-freshworks <54554766+dbayly-freshworks@users.noreply.github.com>
2021-05-12 15:53:20 -0700

    Hcap 536 filter participants for email blast (#371)
    * added query to get email list
    
    * pre-pr cleanup
    
    * more clean up
    
    * added hired restriction
    
    * formatting
    
    * Update server/scripts/get-email-list.js
    
    Co-authored-by: arranfw <71518072+arranfw@users.noreply.github.com>
    
    * Fixed time check and exclude hired entries
    
    * Update server/scripts/get-email-list.js
    
    Co-authored-by: arranfw <71518072+arranfw@users.noreply.github.com>
    
    * re-adding missing quote and parenthesis
    
    * forgot to re-add distinct to query
    
    Co-authored-by: David Bayly <davidbayly@freshworks.io>
    Co-authored-by: arranfw <71518072+arranfw@users.noreply.github.com>

---
+++ server/scripts/get-email-list.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+const { dbClient } = require('../db');
+require('dotenv').config({ path: '../.env' });
+(async function exec() {
+  await dbClient.connect();
+  const res = await dbClient.db.query(`
+    SELECT DISTINCT p.body ->> 'emailAddress' AS email
+    FROM participants p 
+    WHERE 
+        p.body ->> 'emailAddress' NOT IN (
+            SELECT p2.body->> 'emailAddress'
+            FROM participants p2 
+            JOIN participants_status ps
+            ON p2.id = ps.participant_id AND ps.status = 'hired'
+        )
+        AND (CAST(p.body ->> 'interested' AS TEXT) = 'yes'  
+            OR CAST(p.body->> 'interested' AS TEXT) IS NULL)
+        AND (
+            to_timestamp(p.body->>'userUpdatedAt', 'YYYY-MM-DD') < CURRENT_TIMESTAMP - interval '6 weeks'
+        )`
+  );
+  console.log(res);
+  process.exit(0);
+})();

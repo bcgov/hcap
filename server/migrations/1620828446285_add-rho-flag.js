50566c3dbdf873231d526ff087318079bfe8244b
Arran <arran@freshworks.io>
2021-05-12 08:42:17 -0600

    employer_sites.isRHO migration

---
+++ server/migrations/1620828446285_add-rho-flag.js
@@ -0,0 +1,9 @@
+const { collections } = require('../db');
+
+exports.up = async (pgm) => {
+  await pgm.sql(`
+    UPDATE ${collections.EMPLOYER_SITES}
+    SET body = jsonb_set(body::jsonb, '{isRHO}', 'false')
+    WHERE body->>'isRHO' IS NULL;
+  `);
+};

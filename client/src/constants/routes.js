9516a2aed88d5bc7ae6f0adc837123740605694a
dbayly-freshworks <54554766+dbayly-freshworks@users.noreply.github.com>
2021-05-17 12:08:21 -0700

    Update client/src/constants/routes.js
    Co-authored-by: arranfw <71518072+arranfw@users.noreply.github.com>

--- client/src/constants/routes.js
+++ client/src/constants/routes.js
@@ -10,7 +10,7 @@ export default Object.freeze({
   ParticipantForm: '/participant-form',
   ParticipantConfirmation: '/participant-confirmation',
   EmployerConfirmation: '/employer-confirmation',
-  ConfirmInterest: '/participant-otp',
+  ConfirmInterest: '/confirm-interest',
 
   // Private routes
   Admin: '/admin',

f13091f68aa47c26a6152cf55aceeae74c5a9b7d
David Bayly <davidbayly@freshworks.io>
2021-05-25 12:33:27 -0700

    update participant postal code (and also FSA)

--- client/src/constants/validation.js
+++ client/src/constants/validation.js
@@ -358,6 +358,7 @@ export const EditParticipantFormSchema = yup.object().shape({
     .string()
     .required(errorMessage)
     .matches(/^\d{10}$/, 'Phone number must be provided as 10 digits'),
+  postalCode: yup.string().nullable().matches(/^[a-zA-Z]\d[a-zA-Z]\s?\d[a-zA-Z]\d$/,'Invalid postal code'),
   emailAddress: yup.string().required(errorMessage).email('Invalid email address'),
   interested: yup.string().nullable(),
 });

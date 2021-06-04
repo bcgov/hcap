283344609c83c504ec2dba769d7fc6af8af594f5
Arran <arran@freshworks.io>
2021-06-04 17:09:18 -0600

    EOIViewDetails Global Auth

--- client/src/pages/private/EOIViewDetails.js
+++ client/src/pages/private/EOIViewDetails.js
@@ -1,30 +1,17 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import Grid from '@material-ui/core/Grid';
 import { Page, CheckPermissions } from '../../components/generic';
 import { Form } from '../../components/employer-form';
 import { scrollUp } from '../../utils';
 import store from 'store';
 import { API_URL } from '../../constants';
+import { AuthContext } from '../../providers';
 
 export default ({ match }) => {
-  const [roles, setRoles] = useState([]);
   const [user, setUser] = useState(undefined);
-  const [isLoadingUser, setLoadingUser] = useState(true);
+  const { auth } = AuthContext.useAuth();
+  const roles = useMemo(() => auth.user?.roles || [], [auth.user?.roles]);
   const expressionID = match.params.id;
-
-  const fetchUserInfo = async () => {
-    const response = await fetch(`${API_URL}/api/v1/user`, {
-      headers: {
-        Authorization: `Bearer ${store.get('TOKEN')}`,
-      },
-      method: 'GET',
-    });
-
-    if (response.ok) {
-      const { roles } = await response.json();
-      setRoles(roles);
-    }
-  };
 
   useEffect(() => {
     const fetchDetails = async () => {
@@ -37,12 +24,8 @@ export default ({ match }) => {
 
       if (response.ok) {
         setUser(await response.json());
-        setLoadingUser(false);
       }
     };
-
-    setLoadingUser(true);
-    fetchUserInfo();
     fetchDetails();
   }, [expressionID]);
 
@@ -50,7 +33,6 @@ export default ({ match }) => {
   return (
     <Page>
       <CheckPermissions
-        isLoading={isLoadingUser}
         roles={roles}
         permittedRoles={['health_authority', 'ministry_of_health']}
         renderErrorMessage={true}

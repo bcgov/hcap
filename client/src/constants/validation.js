e8f23b21a9a6b878882be91cdf40bc83bf007304
David Bayly <davidbayly@freshworks.io>
2021-06-08 10:30:16 -0700

    fixed test

--- client/src/constants/validation.js
+++ client/src/constants/validation.js
@@ -361,7 +361,7 @@ export const EditParticipantFormSchema = yup.object().shape({
   postalCode: yup
     .string()
     .required('Postal code is required')
-    .test('is-null', 'Postal code is required', (v) => v == null || v === '')
+    .test('is-null', 'Postal code is required!', (v) => v != null && v != '')
     .matches(/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/, 'Invalid postal code'),
   emailAddress: yup.string().required(errorMessage).email('Invalid email address'),
   interested: yup.string().nullable(),

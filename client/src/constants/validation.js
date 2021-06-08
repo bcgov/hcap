6c32ae37819b2bc3e391dac27711936ce8a24613
David Bayly <davidbayly@freshworks.io>
2021-06-08 09:59:07 -0700

    added nullable check for postal code

--- client/src/constants/validation.js
+++ client/src/constants/validation.js
@@ -360,8 +360,8 @@ export const EditParticipantFormSchema = yup.object().shape({
     .matches(/^\d{10}$/, 'Phone number must be provided as 10 digits'),
   postalCode: yup
     .string()
-    .nullable()
     .required('Postal code is required')
+    .test('is-null', 'Postal code is required', (v) => v == null || v === '')
     .matches(/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/, 'Invalid postal code'),
   emailAddress: yup.string().required(errorMessage).email('Invalid email address'),
   interested: yup.string().nullable(),

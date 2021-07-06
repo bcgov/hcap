be5ce09f8d5f17504f341e8eff83271d2ded7a8a
David Bayly <davidbayly@freshworks.io>
2021-07-06 16:07:02 -0700

    Fixed formatting

--- client/src/pages/public/ParticipantForm.js
+++ client/src/pages/public/ParticipantForm.js
@@ -5,7 +5,7 @@ import { Form } from '../../components/participant-form';
 
 export default () => {
   return (
-    <Page hideEmployers ={!window.location.hostname.includes('freshworks.club')}>
+    <Page hideEmployers={!window.location.hostname.includes('freshworks.club')}>
       <Form />
     </Page>
   );

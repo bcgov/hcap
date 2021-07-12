ce3e0ccb20ef3664eefc1b314406e31552fe71dd
fwpushan <pushan@freshworks.io>
2021-07-13 03:35:12 +0530

    HCAP-516/555 | Add new router dir with participant-user routes | Fix Bug HCAP-748

--- client/src/components/participant-form/index.js
+++ client/src/components/participant-form/index.js
@@ -12,7 +12,7 @@ import { Button } from '../generic';
 import { Summary } from './Summary';
 import { Fields } from './Fields';
 
-export const Form = ({ initialValues, isDisabled }) => {
+export const Form = ({ initialValues, isDisabled, hideSummery }) => {
   const history = useHistory();
   const { openToast } = useToast();
   const [submitLoading, setSubmitLoading] = useState(false);
@@ -73,12 +73,12 @@ export const Form = ({ initialValues, isDisabled }) => {
       >
         {({ errors, submitForm, setTouched, values }) => (
           <FormikForm>
-            <Box pt={4} pb={4} pl={2} pr={2}>
+            <Box hidden={hideSummery} pt={4} pb={4} pl={2} pr={2}>
               <Summary />
             </Box>
 
             <Box pt={2} pb={4} pl={2} pr={2}>
-              <Fields isDisabled={isDisabled} />
+              <Fields isDisabled={isDisabled} hideHelp={hideSummery} />
             </Box>
 
             {!isDisabled && (
--- server/server.js
+++ server/server.js
@@ -46,6 +46,9 @@ const { dbClient, collections } = require('./db');
 const { errorHandler, asyncMiddleware } = require('./error-handler.js');
 const keycloak = require('./keycloak.js');
 
+// Routes
+const participantUserRoute = require('./routes/participant-user');
+
 const apiBaseUrl = '/api/v1';
 const app = express();
 
@@ -84,6 +87,9 @@ app.use(
 app.use(keycloak.expressMiddleware());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, '../client/build')));
+
+// Applying router handlers
+app.use(`${apiBaseUrl}/participant-user`, participantUserRoute);
 
 // Return client info for Keycloak realm for the current environment
 app.get(`${apiBaseUrl}/keycloak-realm-client-info`, (req, res) =>

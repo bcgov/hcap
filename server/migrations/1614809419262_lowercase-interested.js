b3fd7e784f1bea235b1c46b431b2280bc315227a
Arran <arran@freshworks.io>
2021-07-27 08:29:54 -0600

    remove extraneous logging for test + migrations

--- server/migrations/1614809419262_lowercase-interested.js
+++ server/migrations/1614809419262_lowercase-interested.js
@@ -2,9 +2,8 @@
 const { dbClient, collections } = require('../db');
 
 exports.up = async () => {
-  const results = await dbClient.db[collections.PARTICIPANTS].updateDoc(
+  await dbClient.db[collections.PARTICIPANTS].updateDoc(
     { interested: 'Yes' },
     { interested: 'yes' }
   );
-  console.log(`Lowercase interested Yes rows affected: ${results.length}`);
 };
--- server/migrations/1616190016615_post-import-cleanup.js
+++ server/migrations/1616190016615_post-import-cleanup.js
@@ -3,29 +3,25 @@ const { dbClient, collections } = require('../db');
 
 exports.up = async () => {
   await dbClient.db.withTransaction(async (tx) => {
-    const postalCodes = await tx[collections.PARTICIPANTS].updateDoc(
+    await tx[collections.PARTICIPANTS].updateDoc(
       {
         postalCode: 'Z1Z1Z1',
       },
       { postalCode: '' }
     );
 
-    const nonHCAPs = await tx[collections.PARTICIPANTS].updateDoc(
+    await tx[collections.PARTICIPANTS].updateDoc(
       {
         nonHCAP: 'NULL',
       },
       { nonHCAP: '' }
     );
 
-    const crcClears = await tx[collections.PARTICIPANTS].updateDoc(
+    await tx[collections.PARTICIPANTS].updateDoc(
       {
         crcClear: 'NULL',
       },
       { crcClear: '' }
     );
-
-    console.log(`Updated ${postalCodes.length} postalCode fields`);
-    console.log(`Updated ${nonHCAPs.length} nonHCAP fields`);
-    console.log(`Updated ${crcClears.length} crcClear fields`);
   });
 };

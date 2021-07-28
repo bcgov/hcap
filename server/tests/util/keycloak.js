943facdd7aa9dc25709e5c4d323eb94aa3d3584e
Pushan Mitra <46456190+fwpushan@users.noreply.github.com>
2021-07-28 21:57:37 +0530

    HCAP-706 | HCAP-801 | Refactor milestone report route (#478)
    ----
    1. Refactor route
    2. Add api e2e tests
    --

--- server/tests/util/keycloak.js
+++ server/tests/util/keycloak.js
@@ -1,9 +1,10 @@
 const axios = require('axios');
 const querystring = require('querystring');
+const logger = require('../../logger');
 
 const superuser = {
   username: 'test-superuser',
-  password: process.env.KC_TEST_SUPER_UER_PWD || 'password',
+  password: process.env.KC_TEST_SUPER_USER_PWD || 'password',
 };
 const employer = {
   username: 'test-employer',
@@ -11,16 +12,24 @@ const employer = {
 };
 
 const getKeycloakToken = async ({ username, password }) => {
-  const data = querystring.stringify({
-    grant_type: 'password',
-    client_id: process.env.KEYCLOAK_FE_CLIENTID,
-    username,
-    password,
-  });
-  const url = `${process.env.KEYCLOAK_AUTH_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`;
-  const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
-  const response = await axios.post(url, data, config);
-  return { Authorization: `Bearer ${response.data.access_token}` };
+  try {
+    const data = querystring.stringify({
+      grant_type: 'password',
+      client_id: process.env.KEYCLOAK_FE_CLIENTID,
+      username,
+      password,
+    });
+    const url = `${process.env.KEYCLOAK_AUTH_URL}/realms/${process.env.KEYCLOAK_REALM}/protocol/openid-connect/token`;
+    const config = { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } };
+    const response = await axios.post(url, data, config);
+    return { Authorization: `Bearer ${response.data.access_token}` };
+  } catch (error) {
+    logger.error({
+      context: `keycloak get token: ${error}`,
+      error,
+    });
+    throw error;
+  }
 };
 
 module.exports = { superuser, employer, getKeycloakToken };

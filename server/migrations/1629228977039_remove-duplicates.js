38b781321ac2198f590492a75930181eb219cfcc
David Bayly <davidbayly@freshworks.io>
2021-08-18 09:50:32 -0700

    Updating migration to use a single query

--- server/migrations/1629228977039_remove-duplicates.js
+++ server/migrations/1629228977039_remove-duplicates.js
@@ -3,28 +3,29 @@
 const { dbClient } = require('../db');
 
 exports.up = async () => {
-  const duplicatedIds = await dbClient.db.query(`
-            SELECT 
-            id
-            from participants_status as p1
-            where current = true AND status IN ('archived','rejected')
-            AND EXISTS(
-                SELECT * from participants_status as p2 
-                WHERE p1.id != p2.id 
-                and p1.id > p2.id 
-                AND p1.status = p2.status 
-                and p1.employer_id = p2.employer_id 
-                and p2.current = TRUE 
-                and p1.participant_id = p2.participant_id
+  await dbClient.db.query(`
+  with ids_to_update as ( 
+    SELECT 
+          id
+          FROM participants_status as p1
+          WHERE 
+      p1.current = true 
+      AND p1.status IN ('archived','rejected')
+            AND EXISTS
+      (
+        SELECT * from participants_status as p2 
+        WHERE p1.id > p2.id 
+          AND p1.status = p2.status 
+          AND p1.employer_id = p2.employer_id 
+          AND p2.current = TRUE 
+          AND p1.participant_id = p2.participant_id
             )
+    )
+    update participants_status 
+    set current = false 
+    from ids_to_update
+    WHERE participants_status.id = ids_to_update.id
         `);
-  await duplicatedIds.forEach(async (duplicate) => {
-    await dbClient.db.query(`
-            UPDATE participants_status
-            SET current = FALSE 
-            WHERE id = ${duplicate.id}
-        `);
-  });
 };
 
 exports.down = () => {};

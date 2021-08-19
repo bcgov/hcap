28f83db7fddb4edcc5dbc76d22ca0962703ea75d
zenen-freshworks <zenen@freshworks.io>
2021-08-19 11:55:19 -0700

    cleaning up tests

--- cypress/integration/participantView.spec.js
+++ cypress/integration/participantView.spec.js
@@ -12,18 +12,6 @@ describe('Participant View', () => {
     cy.get('ul.MuiMenu-list[role=listbox]').should('be.visible');
     cy.get('ul.MuiMenu-list[role=listbox] > li').should('have.length', 3);
 
-    // Testing for freshness
-
-    // cy.get('body').then(($body) => {
-    //   if (Cypress.$('body:contains("a day ago")').length !== 0) {
-    //     cy.contains('a day ago').should('exist');
-    //     cy.contains('14 days ago').should('exist');
-    //   } else {
-    //     cy.contains('2 days ago').should('exist');
-    //     cy.contains('15 days ago').should('exist');
-    //   }
-    // });
-
     // Testing Tabs
     cy.contains('My Sites').should('not.exist');
   });
@@ -59,30 +47,5 @@ describe('Participant View', () => {
     cy.contains('Preferred Location').click();
     cy.get('ul.MuiMenu-list[role=listbox]').should('be.visible');
     cy.get('ul.MuiMenu-list[role=listbox] > li').should('have.length', 6);
-
-    // Testing Tabs - DESCOPED FOR 165
-    // cy.get('ul.MuiMenu-list[role=listbox]').first().click();
-    // cy.get("button#sitesTab").click();
-    // cy.contains("My Sites").should('exist');
   });
-
-  // Disabled for the sake of getting this PR through
-  // it('Uses the MoH edit feature', () => {
-  //   cy.intercept('patch', '/api/v1/participant', (req) => {
-  //     expect(req.body.history[0].changes[0]).to.deep.equal({
-  //       field: 'firstName',
-  //       from: 'Graham',
-  //       to: 'Animal',
-  //     });
-  //     req.reply({ ok: true });
-  //   }).as('patchAnswer');
-  //   cy.kcLogin('test-moh');
-  //   cy.visit('/participant-view');
-  //   cy.contains('Edit').click();
-  //   cy.get('div.MuiDialog-scrollPaper').should('exist');
-  //   cy.get('input[name=firstName').should('have.value', 'Graham').clear().type('Animal');
-  //   cy.contains('Save').focus().click();
-  //   cy.wait('@patchAnswer');
-  //   cy.get('div.MuiDialog-scrollPaper').should('not.exist');
-  // });
 });
--- cypress/integration/siteView.spec.js
+++ cypress/integration/siteView.spec.js
@@ -26,18 +26,18 @@ describe('Tests the Site View', () => {
         cy.get('svg.MuiSvgIcon-root').should('exist');
       });
 
-    // cy.contains('td', 1).should('exist');  // Descoped due to no sites
+    cy.contains('td', 1).should('exist'); // Descoped due to no sites
     cy.get('div.MuiSelect-selectMenu').should('not.have.class', 'Mui-disabled');
     cy.get('div.MuiSelect-selectMenu').click();
     cy.get('ul.MuiMenu-list[role=listbox]').should('be.visible');
     cy.get('ul.MuiMenu-list[role=listbox] > li').should('have.length', 7);
     cy.get('ul.MuiMenu-list[role=listbox] > li').contains('Northern').click();
 
-    // cy.contains('th', 'Site ID').click();
-    // cy.contains('th', 'Site Name').click();
-    // cy.contains('th', 'Operator Name').click();
-    // cy.contains('th', 'Health Authority').click();
-    // cy.contains('th', 'Postal Code').click();
+    cy.contains('th', 'Site ID').click();
+    cy.contains('th', 'Site Name').click();
+    cy.contains('th', 'Operator Name').click();
+    cy.contains('th', 'Health Authority').click();
+    cy.contains('th', 'Postal Code').click();
 
     cy.get('div.MuiSelect-selectMenu').click();
     cy.get('ul.MuiMenu-list[role=listbox] > li').contains('None').click();
@@ -47,14 +47,14 @@ describe('Tests the Site View', () => {
   it('visits siteView as Health Authority', () => {
     cy.kcLogin('test-ha');
     cy.visit('/site-view');
-    // cy.contains('td', 1).should('exist'); // Descoped due to no sites
+    cy.contains('td', 1).should('exist');
     cy.get('div.MuiSelect-selectMenu').should('have.class', 'Mui-disabled');
     cy.get('div.MuiSelect-selectMenu').click();
 
-    // cy.contains('th', 'Site ID').click();
-    // cy.contains('th', 'Site Name').click();
-    // cy.contains('th', 'Operator Name').click();
-    // cy.contains('th', 'Health Authority').click();
-    // cy.contains('th', 'Postal Code').click();
+    cy.contains('th', 'Site ID').click();
+    cy.contains('th', 'Site Name').click();
+    cy.contains('th', 'Operator Name').click();
+    cy.contains('th', 'Health Authority').click();
+    cy.contains('th', 'Postal Code').click();
   });
 });

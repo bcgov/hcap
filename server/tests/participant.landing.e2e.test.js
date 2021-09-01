5c0124767c07e95fc9f6e72e2d95b1c44e84cd62
fwpushan <pushan@freshworks.io>
2021-09-02 00:34:43 +0530

    HCAP-737 | Enable api e2e tests for participant

--- server/tests/participant.landing.e2e.test.js
+++ server/tests/participant.landing.e2e.test.js
@@ -1,16 +1,20 @@
+/**
+ * Tests for route /api/v1/participant-user/*
+ * Test Standalone execution: npm run test:debug participant.landing.e2e.test
+ */
 const request = require('supertest');
 const app = require('../server');
 const { startDB, closeDB } = require('./util/db');
 const { getKeycloakToken, participant } = require('./util/keycloak');
 
-describe('Participant landing e2e', async () => {
+describe('Participant landing e2e', () => {
   let server;
   const form = {
     eligibility: true,
     firstName: 'John',
     lastName: 'Smith',
     phoneNumber: '1234567890',
-    emailAddress: 'test-participant@test.com',
+    emailAddress: 'cristiano.ronaldo@hcap.club',
     postalCode: 'V1V 1V1',
     preferredLocation: ['Fraser'],
     consent: true,
@@ -25,23 +29,23 @@ describe('Participant landing e2e', async () => {
     await server.close();
   });
   // TODO: Re-add these tests when a participant user is added to keycloak in the pipeline
-  it.skip('Get participants request is successful ', async () => {
+  it('should participants - success', async () => {
     const header = await getKeycloakToken(participant);
     const res = await request(app).get('/api/v1/participant-user/participants').set(header);
     expect(res.status).toEqual(200);
   });
-  it.skip('Get participants request is successful ', async () => {
+  it('should get participant data with success', async () => {
     const header = await getKeycloakToken(participant);
     await request.agent(app).post('/api/v1/participants').send(form);
     const res = await request(app).get('/api/v1/participant-user/participants').set(header);
-    expect(res.body.length).toEqual(1);
+    expect(res.body.length).toBeGreaterThan(0);
   });
-  it.skip('Withdraw a participant should succeed', async () => {
+  it('should withdraw a participant', async () => {
     const header = await getKeycloakToken(participant);
     const res = await request(app).post('/api/v1/participant-user/withdraw').set(header);
     expect(res.status).toEqual(204);
   });
-  it.skip('Withdraw a participant should succeed', async () => {
+  it('should withdraw a participant with verification', async () => {
     const header = await getKeycloakToken(participant);
     const res = await request(app).get('/api/v1/participant-user/participants').set(header);
     expect(res.body[0].interested).toEqual('withdrawn');

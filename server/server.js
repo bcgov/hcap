6937d8a7c42609f1bbb7bbcf2ab0ee4875276420
Pushan Mitra <46456190+fwpushan@users.noreply.github.com>
2021-09-28 01:10:29 +0530

    HCAP-970 | Fix security warnings (#561)
    * HCAP-970 | Fix security warnings
    ----
    1. Add or update header to disable security warning
    2. Block options and trace method in express
    
    * HCAP-970 | Keep single cache control
    
    * HCAP-970 | Remove another wild card script source | Disable one inconsistent test, need to setup proper data before testing
    
    * HCAP-970 | Remove some wildcard entry from font-src and style-src
    
    * HCAP-970 | Enable unsafe-inline
    
    * HCAP-970 | Add Pragma header
    
    * Try to fix proxy issue
    
    * Disable all http method excepts get, post, put, patch, delete

--- server/server.js
+++ server/server.js
@@ -9,7 +9,8 @@ const { expressAccessLogger } = require('./middleware');
 
 const apiBaseUrl = '/api/v1';
 const app = express();
-
+app.disable('x-powered-by');
+app.set('trust proxy');
 if (
   process.env.NODE_ENV === 'local' ||
   process.env.NODE_ENV === 'test' ||
@@ -25,26 +26,44 @@ app.use(
         'default-src': ["'self'"],
         'connect-src': [
           "'self'",
-          'https://*.apps.gov.bc.ca',
           'https://orgbook.gov.bc.ca',
-          'https://*.oidc.gov.bc.ca',
+          'https://dev.oidc.gov.bc.ca',
+          'https://test.oidc.gov.bc.ca',
           'https://oidc.gov.bc.ca',
         ],
         'base-uri': ["'self'"],
         'block-all-mixed-content': [],
-        'font-src': ["'self'", 'https:', 'data:'],
+        'font-src': ["'self'"],
         'frame-ancestors': ["'self'"],
         'img-src': ["'self'", 'data:'],
         'object-src': ["'none'"],
-        'script-src': ["'self'", 'https://*.gov.bc.ca'],
+        'script-src': ["'self'"],
         'script-src-attr': ["'none'"],
-        'style-src': ["'self'", 'https:', "'unsafe-inline'"],
+        'style-src': ["'self'", "'unsafe-inline'"],
         'upgrade-insecure-requests': [],
         'form-action': ["'self'"],
       },
     },
   })
 );
+
+// Adding cache control header and xss-protection header
+app.use((req, res, next) => {
+  res.set('Cache-Control', 'no-store');
+  res.set('X-XSS-Protection', '1; mode=block');
+  res.set('Set-Cookie', 'SameSite=Strict');
+  res.set('Pragma', 'no-cache');
+  next();
+});
+
+// Disable Option OPTIONS / TRACE
+app.use((req, res, next) => {
+  const allowedMethods = ['get', 'post', 'put', 'patch', 'delete'];
+  if (!allowedMethods.includes(req.method.toLocaleLowerCase())) {
+    return res.end(405, 'Method is not allowed');
+  }
+  return next();
+});
 
 app.use(expressAccessLogger);
 app.use(bodyParser.json());

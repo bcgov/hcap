ff1e99166c1f3b551b519840f3e7254c847fa1a8
arranfw <71518072+arranfw@users.noreply.github.com>
2021-11-03 12:45:34 -0600

    HCAP-980 | refresh bug (#567)
    * apply nonces with string-replace-middleware
    
    * fix source of buffer error
    
    * final cleanup

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const cors = require('cors');
-const fs = require('fs');
+const { stringReplace } = require('string-replace-middleware');
 const express = require('express');
 const helmet = require('helmet');
 const uuid = require('uuid');
@@ -71,26 +71,33 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   const allowedMethods = ['get', 'post', 'put', 'patch', 'delete'];
   if (!allowedMethods.includes(req.method.toLocaleLowerCase())) {
-    return res.end(405, 'Method is not allowed');
+    return res.status(405).send('Method is not allowed');
   }
   return next();
 });
 
+/**
+ * Apply nonces to static files
+ */
+app.use((req, res, next) => {
+  stringReplace({
+    '<script': `<script nonce='${res.locals.cspNonce}'`,
+    '<style': `<style nonce='${res.locals.cspNonce}'`,
+    __CSP_NONCE__: res.locals.cspNonce,
+  })(req, res, next);
+});
+
 app.use(expressAccessLogger);
+app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.json());
 app.use(`${apiBaseUrl}`, apiRouter);
+
 // Client app
-app.get('/', (req, res) => {
-  const { cspNonce } = res.locals;
 
-  const html = fs.readFileSync(path.join(__dirname, '../client/build/index.html'), 'utf-8');
+if (process.env.NODE_ENV === 'production') {
+  app.get('/*', (req, res) => res.sendFile(path.join(__dirname, '../client/build', '/index.html')));
+}
 
-  let newHTML = html.replace(/<(script|style)/g, `<$1 nonce="${cspNonce}"`);
-  newHTML = newHTML.replace(/__CSP_NONCE__/g, `${cspNonce}`);
-
-  res.send(newHTML);
-});
-app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(errorHandler);
 
 module.exports = app;

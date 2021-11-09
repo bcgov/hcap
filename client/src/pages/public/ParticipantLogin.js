9c171f693983bba7c98bfaebce67515e00420c96
arranfw <71518072+arranfw@users.noreply.github.com>
2021-11-09 09:09:39 -0700

    improve spacing and responsive behaviour (#576)
    simplify style definitions

--- client/src/pages/public/ParticipantLogin.js
+++ client/src/pages/public/ParticipantLogin.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import { Page, Button } from '../../components/generic';
 import { useHistory } from 'react-router-dom';
@@ -9,44 +8,33 @@ import { makeStyles } from '@material-ui/core/styles';
 import { Routes } from '../../constants';
 
 const useStyles = makeStyles((theme) => ({
-  button: {
-    color: 'white',
-    marginTop: '10px',
-    marginBottom: '10px',
-  },
   blueText: {
     color: theme.palette.primary.light,
   },
   blueBox: {
     backgroundColor: '#EDF6FF',
     maxWidth: 554,
-    height: 205,
+  },
+  pageContainer: {
+    [theme.breakpoints.down('md')]: {
+      flexDirection: 'column',
+    },
+    [theme.breakpoints.up('md')]: {
+      flexDirection: 'row',
+    },
   },
   rightContainer: {
-    paddingTop: '80px',
-    [theme.breakpoints.down('md')]: {
-      paddingLeft: '32px',
-      paddingRight: '32px',
-    },
+    padding: '10%',
     [theme.breakpoints.up('md')]: {
-      paddingRight: '160px',
-      paddingLeft: '32px',
+      width: '50%',
     },
     backgroundColor: '#FFFFFF',
-    height: 'calc(100vh - 68px)',
   },
   leftContainer: {
-    paddingTop: '80px',
-    [theme.breakpoints.down('md')]: {
-      paddingLeft: '32px',
-      paddingRight: '32px',
-    },
+    padding: '10%',
     [theme.breakpoints.up('md')]: {
-      paddingLeft: '160px',
-      paddingRight: '32px',
-      alignContent: true,
+      width: '50%',
     },
-    height: 'calc(100vh - 68px)',
   },
 }));
 
@@ -65,19 +53,21 @@ export default () => {
   };
 
   return (
-    <Page hideEmployers={true}>
-      <Grid container>
-        <Grid item md={6} sm={12}>
-          <Box className={classes.leftContainer}>
-            <Box mb={3}>
-              <Typography variant='h2'>
-                <b>Login</b>
-              </Typography>
-            </Box>
+    <Page hideEmployers={true} isAutoHeight={false}>
+      <Box container display='flex' minHeight='100%' className={classes.pageContainer}>
+        <Box className={classes.leftContainer}>
+          <Box mb={3}>
+            <Typography variant='h2'>
+              <b>Login</b>
+            </Typography>
+          </Box>
+          <Box mb={2}>
             <Typography>
               You can use a mobile BC Services Card to log in to the <br /> Health Care Access
               Program
             </Typography>
+          </Box>
+          <Box mb={2}>
             <Button
               className={classes.button}
               onClick={redirectToLogin}
@@ -87,48 +77,48 @@ export default () => {
               style={{ 'text-transform': 'none' }}
               text={'Login with BC Services Card'}
             />
-            <Typography variant={'body2'}>
-              For more information on the BC Services Card, including how to set up your card,
-              please visit the &nbsp;
-              <a href='https://www.gov.bc.ca/mobilebcservicescard'>BC Services Card website.</a>
-            </Typography>
           </Box>
-        </Grid>
-        <Grid item md={6} sm={12}>
-          <Box className={classes.rightContainer}>
-            <Typography variant='subtitle1' className={classes.blueText}>
-              Work in the health care sector
+          <Typography variant={'body2'}>
+            For more information on the BC Services Card, including how to set up your card, please
+            visit the &nbsp;
+            <a href='https://www.gov.bc.ca/mobilebcservicescard'>BC Services Card website.</a>
+          </Typography>
+        </Box>
+        <Box className={classes.rightContainer}>
+          <Typography variant='subtitle1' color='' className={classes.blueText}>
+            Work in the health care sector
+          </Typography>
+          <Box mb={3}>
+            <Typography variant='h2'>Welcome to the Health Career Access Program</Typography>
+          </Box>
+          <Typography mb={2}>
+            COVID-19 has increased the need for health care assistants in long-term care and
+            assisted living settings across the province. The Health Career Access Program provides
+            a path for applicants with no health care experience to get hired and receive paid
+            employer sponsored health care assistant training as part of their employment.
+          </Typography>
+          <Box p={4} mt={2} className={classes.blueBox}>
+            <Typography variant={'subtitle2'} className={classes.blueText}>
+              Don't have an account yet?
             </Typography>
-            <Box mb={3}>
-              <Typography variant='h2'>Welcome to the Health Career Access Program</Typography>
-            </Box>
-            <Typography mb={2}>
-              COVID-19 has increased the need for health care assistants in long-term care and
-              assisted living settings across the province. The Health Career Access Program
-              provides a path for applicants with no health care experience to get hired and receive
-              paid employer sponsored health care assistant training as part of their employment.
-            </Typography>
-            <Box p={4} mt={2} className={classes.blueBox}>
-              <Typography variant={'subtitle2'} className={classes.blueText}>
-                Don't have an account yet?
-              </Typography>
+            <Box mb={2}>
               <Typography>
                 To start as a health care support worker, you need to submit your expression of
                 interest first.
               </Typography>
-              <Button
-                className={classes.button}
-                variant='contained'
-                color='primary'
-                fullWidth={false}
-                text={'Submit Your Expression of Interest'}
-                onClick={redirectToForm}
-                m={2}
-              />
             </Box>
+            <Button
+              className={classes.button}
+              variant='contained'
+              color='primary'
+              fullWidth={false}
+              text={'Submit Your Expression of Interest'}
+              onClick={redirectToForm}
+              m={2}
+            />
           </Box>
-        </Grid>
-      </Grid>
+        </Box>
+      </Box>
     </Page>
   );
 };

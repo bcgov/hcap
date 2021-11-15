d1cb266f748f2fba991770c77aa45dcfb0fa1dbb
Arran <arran@freshworks.io>
2021-11-15 09:05:25 -0700

    Use makeStyles + responsive

--- client/src/components/participant-form/DisabledForm.js
+++ client/src/components/participant-form/DisabledForm.js
@@ -1,103 +1,186 @@
 import React from 'react';
-import store from 'store';
 import * as yup from 'yup';
-import { Typography, Box, Grid, Icon } from '@material-ui/core';
+import { Typography, Box, Icon } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Form } from '../../components/participant-form';
 import { Field, Formik, Form as FormikForm } from 'formik';
 import { RenderTextField } from '../../components/fields';
 import { Button } from '../../components/generic';
 
-import Notifications from '@material-ui/icons/Notifications';
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
 
-export const DisabledForm = ({}) => {
-  const handleSubmit = (values, { setSubmitting }) => {
+const useStyles = makeStyles((theme) => ({
+  container: {
+    position: 'relative',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+  },
+  infoContainer: {
+    top: '100%',
+    transform: 'translateY(-50%)',
+    position: 'absolute',
+    zIndex: '1',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      position: 'static',
+      top: 'auto',
+      transform: 'none',
+      width: '100%',
+    },
+  },
+  infoBox: {
+    background: 'white',
+    padding: '3rem 7.5rem',
+    borderRadius: '.25rem',
+    textAlign: 'center',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: 'clamp(775px, 65%, 1000px)',
+    [theme.breakpoints.down('sm')]: {
+      position: 'static',
+      top: 'auto',
+      transform: 'none',
+      width: 'auto',
+      margin: '1rem',
+      padding: '1rem',
+    },
+  },
+  infoBoxHeader: {
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '2rem',
+      lineHeight: '2.25rem',
+    },
+  },
+  infoBoxParagraph: {
+    fontSize: '16px',
+    marginBottom: '1rem',
+    lineHeight: '24px',
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'left',
+    },
+  },
+  hero: {
+    width: '100%',
+    minHeight: '300px',
+    zIndex: '0',
+    objectFit: 'cover',
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  heading: {
+    position: 'absolute',
+    zIndex: '1',
+    color: 'white',
+    bottom: '100%',
+    marginBottom: '2rem',
+    [theme.breakpoints.down('sm')]: {
+      background: theme.palette.primary.lighter,
+      position: 'static',
+      textAlign: 'center',
+      top: 'auto',
+      transform: 'none',
+      width: '100%',
+      fontSize: '2rem',
+      margin: 0,
+      fontWeight: 'bold',
+      padding: '2rem',
+    },
+  },
+  submissionContainer: {
+    position: 'absolute',
+    zIndex: '1',
+    top: '100%',
+    marginTop: '1rem',
+  },
+  submissionHeader: {
+    color: '#1a5a96',
+    margin: '0 1rem 1rem 1rem',
+    [theme.breakpoints.down('sm')]: {
+      textAlign: 'center',
+      fontSize: '1.5rem',
+      lineHeight: '1.75rem',
+    },
+  },
+  submissionInputContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    flexGrow: '1',
+    width: '20rem',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+    },
+  },
+  submissionForm: {
+    display: 'flex',
+    gap: '1rem',
+    alignItems: 'flex-start',
+    [theme.breakpoints.down('sm')]: {
+      width: 'auto',
+      margin: '1rem',
+      flexDirection: 'column',
+      gap: '0',
+    },
+  },
+  submissionButton: {
+    marginTop: '1rem',
+    minWidth: '8.5rem',
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+    },
+  },
+}));
+
+export const DisabledForm = () => {
+  const classes = useStyles();
+  const handleSubmit = async (values, { setSubmitting }) => {
     console.log('submitted', { values });
+    await new Promise((resolve) => setTimeout(resolve, 500));
     setSubmitting(false);
   };
 
   return (
-    <div
-      style={{
-        position: 'relative',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        width: '100%',
-      }}
-    >
-      <img
-        src='hero.png'
-        alt=''
-        style={{ width: '100%', minHeight: '300px', zIndex: '0', objectFit: 'cover' }}
-      />
+    <Box className={classes.container}>
+      <img src='hero.png' alt='' className={classes.hero} />
 
-      <div
-        style={{
-          top: '100%',
-          transform: 'translateY(-50%)',
-          position: 'absolute',
-          zIndex: '1',
-          background: 'white',
-          padding: '3rem 7.5rem',
-          borderRadius: '.25rem',
-          textAlign: 'center',
-          width: 'clamp(775px, 65%, 1000px)',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Typography
-          variant='h1'
-          style={{
-            position: 'absolute',
-            zIndex: '1',
-            color: 'white',
-            bottom: '100%',
-            marginBottom: '2rem',
-          }}
-        >
+      <div className={classes.infoContainer}>
+        <Typography variant='h1' className={classes.heading}>
           Work In The Health Care Sector
         </Typography>
-        <Box mb={3}>
-          <Typography variant='h2'>Submissions are temporarily closed</Typography>
-        </Box>
-        <div></div>
-        <Typography
-          variant='body1'
-          style={{ fontSize: '16px', marginBottom: '1rem', lineHeight: '24px' }}
-        >
-          The participant expression of interest for the HCAP program is now closed and may reopen
-          as more opportunities become available. Thank you for your interest in the program. To
-          learn more, visit the{' '}
-          <a href='https://gov.bc.ca/careersinhealth' target='_blank' rel='noreferrer'>
-            Health Career Access Program
-          </a>
-          .
-        </Typography>
-        <Typography
-          variant='body1'
-          style={{ fontSize: '16px', marginBottom: '1rem', lineHeight: '24px' }}
-        >
-          Eligible employers will continue to reach out to those who are currently in the system. If
-          you have any questions, please email{' '}
-          <a href='mailto:HCAPInfoQuery@gov.bc.ca' target='_blank' rel='noreferrer'>
-            HCAPInfoQuery@gov.bc.ca
-          </a>
-          .
-        </Typography>
+        <Box className={classes.infoBox}>
+          <Box mb={3}>
+            <Typography variant='h2' className={classes.infoBoxHeader}>
+              Submissions are temporarily closed
+            </Typography>
+          </Box>
 
-        <Box
-          style={{
-            position: 'absolute',
-            zIndex: '1',
-            top: '100%',
-            marginTop: '1rem',
-          }}
-        >
-          <Typography variant='subtitle1' style={{ color: '#1a5a96', marginBottom: '1rem' }}>
+          <Typography variant='body1' className={classes.infoBoxParagraph}>
+            The participant expression of interest for the HCAP program is now closed and may reopen
+            as more opportunities become available. Thank you for your interest in the program. To
+            learn more, visit the{' '}
+            <a href='https://gov.bc.ca/careersinhealth' target='_blank' rel='noreferrer'>
+              Health Career Access Program
+            </a>
+            .
+          </Typography>
+          <Typography variant='body1' className={classes.infoBoxParagraph}>
+            Eligible employers will continue to reach out to those who are currently in the system.
+            If you have any questions, please email{' '}
+            <a href='mailto:HCAPInfoQuery@gov.bc.ca' target='_blank' rel='noreferrer'>
+              HCAPInfoQuery@gov.bc.ca
+            </a>
+            .
+          </Typography>
+        </Box>
+
+        <Box className={classes.submissionContainer}>
+          <Typography variant='subtitle1' className={classes.submissionHeader}>
             Get notified when the submissions are open
           </Typography>
           <Formik
@@ -107,22 +190,9 @@ export const DisabledForm = ({}) => {
             })}
             onSubmit={handleSubmit}
           >
-            <FormikForm>
-              <Box
-                style={{
-                  display: 'flex',
-                  gap: '1rem',
-                  alignItems: 'flex-end',
-                }}
-              >
-                <Box
-                  style={{
-                    display: 'flex',
-                    flexDirection: 'column',
-                    alignItems: 'flex-start',
-                    flexGrow: '1',
-                  }}
-                >
+            {({ isSubmitting }) => (
+              <FormikForm className={classes.submissionForm}>
+                <Box className={classes.submissionInputContainer}>
                   <Field
                     name='email'
                     component={RenderTextField}
@@ -134,20 +204,21 @@ export const DisabledForm = ({}) => {
                   type='submit'
                   variant='contained'
                   color='primary'
+                  loading={isSubmitting}
+                  className={classes.submissionButton}
                   text={
-                    <>
-                      <Icon mar component={Notifications} />
+                    <Box display='flex' alignItems='center'>
+                      <Icon component={NotificationsActiveIcon} style={{ marginRight: '.5rem' }} />
                       Notify me
-                    </>
+                    </Box>
                   }
                   fullWidth={false}
-                  style={{ marginTop: '1rem' }}
                 />
-              </Box>
-            </FormikForm>
+              </FormikForm>
+            )}
           </Formik>
         </Box>
       </div>
-    </div>
+    </Box>
   );
 };

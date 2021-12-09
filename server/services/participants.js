b41ca3109c31c74dafd6e92d9cdf4cd611d80275
David Bayly <dbayly@freshworks.io>
2021-12-09 08:41:16 -0800

    Added filter to prevent hired PEOIs from being withdrawn

--- server/services/participants.js
+++ server/services/participants.js
@@ -735,7 +735,8 @@ const withdrawParticipantsByEmail = async (userId, email) => {
   await dbClient.db.withTransaction(async (tx) => {
     const participants = await getParticipantsForUser(userId, email);
     await participants.forEach(async (participant) => {
-      if (participant.interested === 'withdrawn') {
+      participant;
+      if (participant.interested === 'withdrawn' || participant.hired.length) {
         return;
       }
       const historyObj = {

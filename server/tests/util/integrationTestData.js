9ec76bf14e51e0588394a1d2ba162f7b1debb2ef
fwpushan <pushan@freshworks.io>
2022-02-15 00:07:17 +0530

    HCAP-1066 | HCAP-1038 | Cohort Assignment Validation While Assigning Graduation Status

--- server/tests/util/integrationTestData.js
+++ server/tests/util/integrationTestData.js
@@ -1,7 +1,8 @@
-const { participantData } = require('./testData');
+const { participantData, psiData, cohortData } = require('./testData');
 const { makeParticipant } = require('../../services/participants');
-// Subjects
 const { createPostHireStatus } = require('../../services/post-hire-flow');
+const { makePSI } = require('../../services/post-secondary-institutes');
+const { makeCohort, assignCohort } = require('../../services/cohorts');
 
 const makeTestPostHireStatus = async ({ email, status, data = {} }) => {
   const participantObj = participantData({ emailAddress: email });
@@ -16,7 +17,46 @@ const makeTestPostHireStatus = async ({ email, status, data = {} }) => {
 
 const makeTestParticipant = async (options) => makeParticipant(participantData(options));
 
+const makeCohortAssignment = async ({
+  cohortName,
+  cohortId,
+  email,
+  participantId,
+  psiName,
+  psiId,
+}) => {
+  let finalParticipantId = participantId;
+  let finalPSIId = psiId;
+  let finalCohortId = cohortId;
+  if (email) {
+    const participant = await makeTestParticipant({ emailAddress: email });
+    finalParticipantId = participant.id;
+  }
+  if (psiName) {
+    const psiDataObj = psiData({ instituteName: psiName });
+    const psi = await makePSI(psiDataObj);
+    finalPSIId = psi.id;
+  }
+  if (cohortName) {
+    const cohortDataObj = cohortData({ cohortName, psiID: finalPSIId, cohortSize: 1 });
+    const cohort = await makeCohort(cohortDataObj);
+    finalCohortId = cohort.id;
+  }
+  const cohortAssignment = await assignCohort({
+    id: finalCohortId,
+    participantId: finalParticipantId,
+  });
+
+  return {
+    participantId: finalParticipantId,
+    psiId: finalPSIId,
+    cohortId: finalCohortId,
+    cohortAssignmentId: cohortAssignment.id,
+  };
+};
+
 module.exports = {
   makeTestParticipant,
   makeTestPostHireStatus,
+  makeCohortAssignment,
 };

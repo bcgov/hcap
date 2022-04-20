aaa6721b49dcc910cf4eb266b6c01bedcc9cb818
Alva <alva@freshworks.io>
2022-04-19 17:17:18 -0700

    some updates to the prospecting form

--- client/src/components/modal-forms/ProspectingForm.js
+++ client/src/components/modal-forms/ProspectingForm.js
@@ -1,26 +1,43 @@
 import React, { Fragment } from 'react';
-import Grid from '@material-ui/core/Grid';
+
+import { Box, Divider, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 import { Button } from '../generic';
-import { Box } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  formText: {
+    fontWeight: 400,
+  },
+  formButton: {
+    maxWidth: '200px',
+  },
+  formDivider: {
+    marginBottom: theme.spacing(2),
+    marginTop: theme.spacing(4),
+  },
+}));
 
 export const ProspectingForm = ({ name, onClose, onSubmit }) => {
+  const classes = useStyles();
+
   return (
     <Fragment>
-      <Box>{name} has been engaged. This candidate can now be found in the My Candidates tab.</Box>
-      <Box mt={3}>
-        <Grid container spacing={2} justify='flex-end'>
-          <Grid item>
-            <Button onClick={onClose} color='default' text='Close' />
-          </Grid>
-          <Grid item>
-            <Button
-              onClick={onSubmit}
-              variant='contained'
-              color='primary'
-              text='View My Candidates'
-            />
-          </Grid>
-        </Grid>
+      <Typography variant='subtitle2' className={classes.formText}>
+        {name} has been engaged. This candidate can now be found in the My Candidates tab.
+      </Typography>
+
+      <Divider className={classes.formDivider} />
+
+      <Box display='flex' justifyContent='space-between'>
+        <Button className={classes.formButton} onClick={onClose} variant='outlined' text='Close' />
+        <Button
+          className={classes.formButton}
+          onClick={onSubmit}
+          variant='contained'
+          color='primary'
+          text='View My Candidates'
+        />
       </Box>
     </Fragment>
   );

fb8025cfa98d79a56e1b21c24d2055d7ca173523
Tara Epp <tara@freshworks.io>
2022-04-29 10:46:59 -0600

    HCAP-1125 | extend dayjs with utc plugin

--- client/src/utils/date.js
+++ client/src/utils/date.js
@@ -1,7 +1,9 @@
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
+import utc from 'dayjs/plugin/utc';
 
 dayjs.extend(relativeTime);
+dayjs.extend(utc);
 
 export const dayUtils = dayjs;
 

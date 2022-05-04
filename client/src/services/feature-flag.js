15f9a176eeea1a0d683ecb9aca5b4495cb01c06b
Alva <alva@freshworks.io>
2022-05-04 16:11:59 -0700

    changed SelectProspectingSite to single select

--- client/src/services/feature-flag.js
+++ client/src/services/feature-flag.js
@@ -1,4 +1,4 @@
 import store from 'store';
 
 export const FEATURE_MULTI_ORG_PROSPECTING = 'FEATURE_MULTI_ORG_PROSPECTING';
-export const featureFlag = (key) => store.get(key) || false;
+export const featureFlag = (key) => true;

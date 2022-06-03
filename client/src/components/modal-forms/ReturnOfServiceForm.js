737dbea46e23538a216742e39315fb3bcec4df80
fwpushan <pushan@freshworks.io>
2022-06-03 23:50:24 +0530

    HCAP-1084 | Fix code lint

--- client/src/components/modal-forms/ReturnOfServiceForm.js
+++ client/src/components/modal-forms/ReturnOfServiceForm.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import MuiAlert from '@material-ui/lab/Alert';
 import { Button } from '../generic';
-import { Box, Typography } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import {
   RenderDateField,
   RenderRadioGroup,

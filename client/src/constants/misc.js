0f7240195a98c557b5684e6c3a9d2ce63a5f461d
Alva <64768811+alva-fresh@users.noreply.github.com>
2022-06-27 08:40:08 -0700

    some updates to the employer form (#753)

--- client/src/constants/misc.js
+++ client/src/constants/misc.js
@@ -2,3 +2,6 @@ export const API_URL = process.env.REACT_APP_API_URL || '';
 
 export const DOWNLOAD_DEFAULT_ERROR_MESSAGE = 'Error while downloading report';
 export const DOWNLOAD_DEFAULT_SUCCESS_MESSAGE = 'Report generated successfully!';
+
+export const HCAP_LINK =
+  'https://www2.gov.bc.ca/gov/content/employment-business/job-seekers-employees/find-a-job/health-care';

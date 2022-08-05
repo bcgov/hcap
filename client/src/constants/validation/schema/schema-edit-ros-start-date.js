5866c5736743c57d74dd3a274212cfa63d048f6e
Alva <alva@freshworks.io>
2022-08-05 15:12:47 -0700

    validation fixes

--- client/src/constants/validation/schema/schema-edit-ros-start-date.js
+++ client/src/constants/validation/schema/schema-edit-ros-start-date.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 export const EditRosStartDateSchema = yup.object().shape({
-  date: yup
+  startDate: yup
     .date()
     .required('Please enter the start date at a new site')
     .typeError('Invalid Date. Please follow the format: YYYY/MM/DD'),

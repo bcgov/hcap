1952c3e5d45159ee7c3a4fd0874c9952722ae877
Alva <alva@freshworks.io>
2022-08-05 15:12:47 -0700

    validation fixes

--- client/src/constants/validation/schema/schema-edit-ros-site.js
+++ client/src/constants/validation/schema/schema-edit-ros-site.js
@@ -1,5 +1,5 @@
 import * as yup from 'yup';
 
 export const EditRosSiteSchema = yup.object().shape({
-  site: yup.number('Invalid type for the new site').required('New site name is required'),
+  siteName: yup.number('Invalid type for the new site').required('New site name is required'),
 });

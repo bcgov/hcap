9bba95757b547bba40cc2d0f901481bf05da18f6
Tara Epp <tara@freshworks.io>
2022-08-22 09:36:09 -0600

    Added is_current filter to ros status in participant status infos view

--- server/migrations/1660318202044_update_participant_status_infos.js
+++ server/migrations/1660318202044_update_participant_status_infos.js
@@ -13,7 +13,7 @@ exports.up = async () => {
      jsonb_agg(ps.*) FILTER (WHERE ps.id IS NOT NULL) AS status_infos,
      jsonb_agg(DISTINCT jsonb_build_object('id', rosstatus.id, 'data', rosstatus.data, 'status', rosstatus.status, 'site_id', rosstatus.site_id, 'rosSite', ros_site.*)) FILTER (WHERE rosstatus.id IS NOT NULL) AS ros_infos
     FROM participants p
-      LEFT JOIN return_of_service_status rosstatus ON p.id = rosstatus.participant_id
+      LEFT JOIN return_of_service_status rosstatus ON p.id = rosstatus.participant_id AND rosstatus.is_current IS TRUE
       LEFT JOIN participants_status ps ON p.id = ps.participant_id AND ps.current IS TRUE
       LEFT JOIN employer_sites ros_site ON rosstatus.site_id = ros_site.id
    GROUP BY p.id, p.body, p.search, p.created_at, p.updated_at`);

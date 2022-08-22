c90bda569e7cf1529cad8c0d47fa5ed343d527a5
arranfw <arran@freshworks.io>
2022-08-22 16:26:00 -0600

    refactor: remove deprecated feature flag

--- server/services/feature-flags.js
+++ server/services/feature-flags.js
@@ -1,6 +1,6 @@
 const strToBoolean = (value) => value === 'true';
 
 module.exports = {
-  FEATURE_MULTI_ORG_PROSPECTING: strToBoolean(process.env.FEATURE_MULTI_ORG_PROSPECTING) || false,
+  EXAMPLE_USAGE: strToBoolean(process.env.EXAMPLE_USAGE) || false,
   DISABLE_EMPLOYER_FORM: true,
 };

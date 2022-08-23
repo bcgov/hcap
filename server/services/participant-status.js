51772f1fbc0453830b08f1d00adae2818287b840
Noel Feliciano <noel.feliciano@ca.ey.com>
2022-08-22 17:15:24 -0700

    fix: null protection

--- server/services/participant-status.js
+++ server/services/participant-status.js
@@ -102,7 +102,7 @@ const setParticipantStatus = async (
       : await tx[collections.PARTICIPANTS_STATUS].findOne({
           participant_id: participantId,
           current: true,
-          or: [{ employer_id: employerId }, { 'data.site IN': user.sites }],
+          or: [{ employer_id: employerId }, { 'data.site IN': user.sites || [] }],
         });
 
     // If existing status if not current, then invalid status transition

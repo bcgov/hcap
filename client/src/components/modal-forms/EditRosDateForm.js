526f0cbe3f297a81df70e97e6e72e06e99308c1a
arranfw <arran@freshworks.io>
2022-08-29 09:10:37 -0700

    refactor: replace EditRosTemplate with useFormConfirmation

--- client/src/components/modal-forms/EditRosDateForm.js
+++ client/src/components/modal-forms/EditRosDateForm.js
@@ -3,26 +3,27 @@ import React from 'react';
 import { Box } from '@material-ui/core';
 import { Formik, Form as FormikForm, FastField } from 'formik';
 
-import { EditRosTemplate } from './form-components';
+import { FormButtons } from './form-components';
 import { RenderDateField } from '../fields';
+import { useFormConfirmation } from '../../hooks/useFormConfirmation';
+import { rosEditWarning } from '../../constants';
 
 export const EditRosDateForm = ({ initialValues, onSubmit, onClose, validationSchema }) => {
+  const [openConfirmation, FormConfirmation] = useFormConfirmation(onSubmit, rosEditWarning);
+
   return (
-    <Formik initialValues={initialValues} validationSchema={validationSchema}>
-      {({ validateForm, values }) => (
-        <FormikForm>
-          <EditRosTemplate
-            onSubmit={onSubmit}
-            onClose={onClose}
-            values={values}
-            validateForm={validateForm}
-          >
-            <Box my={1}>
-              <FastField name='date' component={RenderDateField} label='Start Date' boldLabel />
-            </Box>
-          </EditRosTemplate>
-        </FormikForm>
-      )}
+    <Formik
+      onSubmit={openConfirmation}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+    >
+      <FormikForm>
+        <Box my={1}>
+          <FastField name='date' component={RenderDateField} label='Start Date' boldLabel />
+        </Box>
+        <FormButtons onClose={onClose} />
+        <FormConfirmation />
+      </FormikForm>
     </Formik>
   );
 };
--- client/src/components/modal-forms/form-components/index.js
+++ client/src/components/modal-forms/form-components/index.js
@@ -1,3 +1,2 @@
 export * from './ConfirmationDialog';
 export * from './FormButtons';
-export * from './EditRosTemplate';

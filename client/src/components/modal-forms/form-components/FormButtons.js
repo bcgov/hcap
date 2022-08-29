77b239c57bf9e24dafae3042f73387b9ea8e718c
arranfw <arran@freshworks.io>
2022-08-29 09:09:22 -0700

    refactor: form button is type=submit,  includes divider

--- client/src/components/modal-forms/form-components/FormButtons.js
+++ client/src/components/modal-forms/form-components/FormButtons.js
@@ -1,13 +1,17 @@
 import React from 'react';
 
-import { Box } from '@material-ui/core';
+import { Box, Divider } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { Button } from '../../generic';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   formButton: {
     maxWidth: '200px',
+  },
+  formDivider: {
+    marginBottom: theme.spacing(3),
+    marginTop: theme.spacing(3),
   },
 }));
 
@@ -15,9 +19,12 @@ export const FormButtons = ({ onSubmit, onClose }) => {
   const classes = useStyles();
 
   return (
-    <Box display='flex' justifyContent='space-between'>
-      <Button className={classes.formButton} onClick={onClose} variant='outlined' text='Cancel' />
-      <Button className={classes.formButton} onClick={onSubmit} text='Confirm' type='submit' />
-    </Box>
+    <>
+      <Divider className={classes.formDivider} />
+      <Box display='flex' justifyContent='space-between'>
+        <Button className={classes.formButton} onClick={onClose} variant='outlined' text='Cancel' />
+        <Button className={classes.formButton} text='Confirm' type='submit' />
+      </Box>
+    </>
   );
 };

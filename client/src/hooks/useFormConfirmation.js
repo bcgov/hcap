968c41c8e4d8598df169b4aaabe51ed7e3f17eda
arranfw <arran@freshworks.io>
2022-08-29 09:08:22 -0700

    feat: useFormConfirmation hook

---
+++ client/src/hooks/useFormConfirmation.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import { useState } from 'react';
+import { ConfirmationDialog } from '../components/modal-forms/form-components';
+
+/**
+ * Given a function to call on confirmation and a warning message, this hook
+ * returns a function to open the confirmation and the confirmation component.
+ *
+ * This function handles the open state for the confirmation a well as calling
+ * the form's submit handler
+ *
+ * @param {*} handleConfirmation function to call when confirmation is affirmed
+ * @param {*} warningMessage message to display in the confirmation dialog
+ * @returns an array containing a function to open the confirmation and a confirmation component
+ */
+export const useFormConfirmation = (handleConfirmation, warningMessage) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [values, setValues] = useState(null);
+
+  const openConfirmation = (values) => {
+    setValues(values);
+    setIsOpen(true);
+  };
+  const closeConfirmation = () => setIsOpen(false);
+
+  const FormConfirmation = () => (
+    <ConfirmationDialog
+      isOpen={isOpen}
+      onClose={closeConfirmation}
+      handleConfirmation={() => handleConfirmation(values)}
+      warningMessage={warningMessage}
+    />
+  );
+
+  return [openConfirmation, FormConfirmation];
+};

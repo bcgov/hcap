f02e390b928b257605f2ec015844a80814e09502
arranfw <arran@freshworks.io>
2022-09-13 14:28:15 -0600

    refactor: remove unused import

--- client/src/pages/private/UserView.js
+++ client/src/pages/private/UserView.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import _orderBy from 'lodash/orderBy';
 import { useHistory } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import { Box, Typography } from '@material-ui/core';

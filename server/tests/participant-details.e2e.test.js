49788bc118b4fa7091e157497b206916a722fd51
arranfw <arran@freshworks.io>
2022-09-15 14:00:57 -0600

    test: fix and add participant access tests

--- server/tests/participant-details.e2e.test.js
+++ server/tests/participant-details.e2e.test.js
@@ -2,7 +2,7 @@
 const request = require('supertest');
 const app = require('../server');
 const { startDB, closeDB } = require('./util/db');
-const { makeTestParticipant } = require('./util/integrationTestData');
+const { makeTestParticipant, createTestParticipantStatus } = require('./util/integrationTestData');
 const { getKeycloakToken, healthAuthority, superuser } = require('./util/keycloak');
 
 describe('e2e tests for /participant/details route', () => {
@@ -26,12 +26,29 @@ describe('e2e tests for /participant/details route', () => {
     expect(res.status).toEqual(200);
   });
 
-  it('should not return participant for HA if participant is not hired or engaged by HA', async () => {
-    const p = await makeTestParticipant({ emailAddress: 'test.e2e.participant.details.2@hcap.io' });
-    expect(p.id).toBeDefined();
+  it('should not return participant for HA if participant is not in the same region', async () => {
+    const { participant } = await createTestParticipantStatus({
+      participantData: { emailAddress: 'test.e2e.participant.details.2@hcap.io' },
+      siteData: { siteId: 1231, healthAuthority: 'Northern' },
+      status: 'hired',
+    });
+    expect(participant.id).toBeDefined();
 
     const header = await getKeycloakToken(healthAuthority);
-    const res = await request(app).get(`/api/v1/participant/details/${p.id}`).set(header);
+    const res = await request(app).get(`/api/v1/participant/details/${participant.id}`).set(header);
     expect(res.status).toEqual(403);
+  });
+
+  it('should return participant for HA if participant is in the same region', async () => {
+    const { participant } = await createTestParticipantStatus({
+      participantData: { emailAddress: 'test.e2e.participant.details.2@hcap.io' },
+      siteData: { siteId: 1232, healthAuthority: 'Fraser' },
+      status: 'hired',
+    });
+    expect(participant.id).toBeDefined();
+
+    const header = await getKeycloakToken(healthAuthority);
+    const res = await request(app).get(`/api/v1/participant/details/${participant.id}`).set(header);
+    expect(res.status).toEqual(200);
   });
 });

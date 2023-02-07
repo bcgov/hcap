07905fe485d54dc217a7cd32a58097935663de88
Meredith Mumby <Meredith.Mumby@ca.ey.com>
2023-02-07 14:20:55 -0700

    Added a max character validation to the phraseName

--- client/src/constants/validation/schema/schema-create-phase.js
+++ client/src/constants/validation/schema/schema-create-phase.js
@@ -2,7 +2,7 @@ import * as yup from 'yup';
 import { validateDateIsReasonable, errorMessage, errorDateIsReasonable } from '../functions';
 
 export const CreatePhaseSchema = yup.object().shape({
-  phaseName: yup.string().required('Phase name is required'),
+  phaseName: yup.string().required('Phase name is required').max(255, 'Phase name is too long'),
   startDate: yup
     .date()
     .required(errorMessage)

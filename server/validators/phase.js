e1398953571d719f3f77cbe6de5e800f0deffd20
Meredith Mumby <Meredith.Mumby@ca.ey.com>
2023-02-17 16:35:01 -0700

    added phase date validation logic to a common object

--- server/validators/phase.js
+++ server/validators/phase.js
@@ -1,6 +1,26 @@
 const yup = require('yup');
 const { validISODateString } = require('./helpers');
 
+const dateValidation = {
+  start_date: yup
+    .string()
+    .required('Start date is required')
+    .test('is-date', 'Not a valid date', validISODateString),
+  end_date: yup
+    .string()
+    .when('start_date', (startDate, schema) => {
+      if (startDate) {
+        return schema.test(
+          'is-after-start',
+          'Invalid entry. End date must be at least 1 day after Start date',
+          (v) => Date.parse(startDate) + 86400000 <= Date.parse(v)
+        );
+      }
+      return schema;
+    })
+    .required('End date is required')
+    .test('is-date', 'Not a valid date', validISODateString),
+};
 const CreatePhaseSchema = yup
   .object()
   .noUnknown('Unknown field in entry')
@@ -9,48 +29,14 @@ const CreatePhaseSchema = yup
       .string()
       .required('Phase name is required')
       .max(255, 'Phase name should be no longer than 255 characters'),
-    start_date: yup
-      .string()
-      .required('Start date is required')
-      .test('is-date', 'Not a valid date', validISODateString),
-    end_date: yup
-      .string()
-      .when('start_date', (startDate, schema) => {
-        if (startDate) {
-          return schema.test(
-            'is-after-start',
-            'Invalid entry. End date must be at least 1 day after Start date',
-            (v) => Date.parse(startDate) + 86400000 <= Date.parse(v)
-          );
-        }
-        return schema;
-      })
-      .required('End date is required')
-      .test('is-date', 'Not a valid date', validISODateString),
+    ...dateValidation,
   });
 
 const UpdatePhaseSchema = yup
   .object()
   .noUnknown('Unknown field in entry')
   .shape({
-    start_date: yup
-      .string()
-      .required('Start date is required')
-      .test('is-date', 'Not a valid date', validISODateString),
-    end_date: yup
-      .string()
-      .when('start_date', (startDate, schema) => {
-        if (startDate) {
-          return schema.test(
-            'is-after-start',
-            'Invalid entry. End date must be at least 1 day after Start date',
-            (v) => Date.parse(startDate) + 86400000 <= Date.parse(v)
-          );
-        }
-        return schema;
-      })
-      .required('End date is required')
-      .test('is-date', 'Not a valid date', validISODateString),
+    ...dateValidation,
   });
 
 module.exports = { CreatePhaseSchema, UpdatePhaseSchema };

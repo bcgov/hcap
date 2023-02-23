4563a4289aa8f8f7a681962c964f4c9346e32c91
Raelynn Hattie <raelynn.hattie@ca.ey.com>
2023-02-23 11:55:05 -0700

    Fixed post hire validation bug

--- server/validators/participant.ts
+++ server/validators/participant.ts
@@ -143,9 +143,7 @@ export const ParticipantPostHireStatusSchema = yup
   .shape({
     participantId: yup.number().required('Participant ID is required'),
     status: yup.string().oneOf(postHireStatusesValues, 'Invalid status'),
-    // See https://github.com/jquense/yup/issues/1901 for why this is needed
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    data: yup.object().when('status', (status: any, schema) => {
+    data: yup.object().when('status', ([status], schema) => {
       switch (status) {
         case postHireStatuses.postSecondaryEducationCompleted:
           return schema.noUnknown('Unknown field in data form').shape({

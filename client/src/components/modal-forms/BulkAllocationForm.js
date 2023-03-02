1ca3cd027b630f34e937f09678cfcc36a41dea32
Meredith Mumby <Meredith.Mumby@ca.ey.com>
2023-03-02 09:43:32 -0700

    fixed linting error

--- client/src/components/modal-forms/BulkAllocationForm.js
+++ client/src/components/modal-forms/BulkAllocationForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Dialog } from '../generic';
 import { Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { RenderTextField } from '../fields';
+import { RenderTextField, RenderSelectField } from '../fields';
 import { Field, Formik, Form as FormikForm } from 'formik';
 
 const useStyles = makeStyles(() => ({
@@ -14,20 +14,38 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-export const BulkAllocationForm = ({ onClose, open, sites }) => {
+export const BulkAllocationForm = ({ onClose, open, sites, phases }) => {
   const classes = useStyles();
+  const siteIds = sites.filter(({ id }) => id);
   const initialValues = {
+    siteIds,
     allocation: '',
+    phase_id: '',
   };
 
   const handleSubmit = async () => {
     console.log('selected sites', sites);
+    console.log(initialValues);
   };
+
   return (
     <Dialog title='Set Allocation' open={open} onClose={onClose}>
       <Formik initialValues={initialValues} onSubmit={handleSubmit}>
         {({ submitForm }) => (
           <FormikForm>
+            <Box>
+              <Field
+                name='phase_id'
+                type='number'
+                component={RenderSelectField}
+                placeholder='Select phase'
+                label='* Phase name'
+                options={[]}
+                onChange={({ target }) => {
+                  console.log(target.value);
+                }}
+              />
+            </Box>
             <Box>
               <Field
                 name='allocation'

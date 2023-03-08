a0752ea44b6bbcf0fb5a0786b10044cec908042b
Meredith Mumby <Meredith.Mumby@ca.ey.com>
2023-03-08 11:00:37 -0700

    updated cypress tests to add a phase before setting bulk allocation, updated row count by couting checkboxes instead

--- cypress/integration/setBulkAllocation.spec.js
+++ cypress/integration/setBulkAllocation.spec.js
@@ -1,5 +1,6 @@
 describe('Bulk Allocation functionality', () => {
   before(() => {});
+
   beforeEach(() => {
     cy.kcLogin('test-moh');
   });
@@ -8,10 +9,27 @@ describe('Bulk Allocation functionality', () => {
     cy.kcLogout();
   });
 
-  const setBulkAllocationForm = (allocation) => {
+  const createPhase = ({ phaseName, startDate, endDate }) => {
+    cy.visit('site-view');
+    cy.contains('button', 'Action').click();
+    cy.contains('li', 'Create new phase').click();
+
+    cy.get('[name=phaseName]').clear().type(phaseName);
+    // the MUI date component does not allow users to type, so cypress needs to mock a copy/paste keyboard action
+    cy.get('[name=Startdate]').clear().type(`{ctrl+v}${startDate}`);
+    cy.get('[name=Enddate]').clear().type(`{ctrl+v}${endDate}`);
+
+    cy.contains('button', 'Create').click();
+  };
+
+  const setBulkAllocationForm = (allocation, override = false) => {
     cy.get('[name=phase_id]').parent().click();
-    cy.get('li').eq(1).click();
+    cy.get('li').contains('Bulk Allocation Testing Phase').click();
     cy.get('[name=allocation]').clear().type(allocation);
+
+    if (override) {
+      cy.get('[name=acknowledgement]').check();
+    }
 
     cy.contains('button', 'Set').click({ force: true });
     cy.get('form').submit();
@@ -23,17 +41,24 @@ describe('Bulk Allocation functionality', () => {
   };
 
   it('MoH can set bulk allocations', () => {
+    const phaseData = {
+      phaseName: 'Bulk Allocation Testing Phase',
+      startDate: '2023/01/01',
+      endDate: '2024/01/01',
+    };
+    // create phase - use new phase to set allocations
+    createPhase(phaseData);
     cy.visit('site-view');
-    cy.get('tbody tr')
+    navigateToFormSelectAll();
+    cy.get('tr td')
+      .find('[type="checkbox"]')
       .its('length')
       .then((n) => {
         cy.log(n);
-        navigateToFormSelectAll();
         setBulkAllocationForm(90);
         // expect: no errors, success message.
         cy.contains('.Mui-error').should('not.exist');
-        // remove table header row from count
-        cy.get('.MuiAlert-message').contains(`${n - 1} sites have been assigned allocations`);
+        cy.get('.MuiAlert-message').contains(`${n} sites have been assigned allocations`);
       });
   });
 
@@ -41,7 +66,6 @@ describe('Bulk Allocation functionality', () => {
     cy.visit('site-view');
     navigateToFormSelectAll();
 
-    // attempt to
     cy.contains('button', 'Set').click({ force: true });
     cy.get('form').submit();
 
@@ -64,20 +88,20 @@ describe('Bulk Allocation functionality', () => {
 
   it('MOH overrides sites with existing allocations', () => {
     cy.visit('site-view');
-    cy.get('tbody tr')
+    navigateToFormSelectAll();
+    cy.get('tr td')
+      .find('[type="checkbox"]')
       .its('length')
       .then((n) => {
         cy.log(n);
-        navigateToFormSelectAll();
-        setBulkAllocationForm(200);
-        cy.get('[name=acknowledgement]').should('exist');
-        cy.get('[name=acknowledgement]').check();
+        const override = true;
+        setBulkAllocationForm(200, override);
         cy.get('form').submit();
 
         // expect: no errors, success message.
+        cy.get('[name=acknowledgement]').should('exist');
         cy.contains('.Mui-error').should('not.exist');
-        // remove table header row from count
-        cy.get('.MuiAlert-message').contains(`${n - 1} sites have been assigned allocations`);
+        cy.get('.MuiAlert-message').contains(`${n} sites have been assigned allocations`);
       });
   });
 });

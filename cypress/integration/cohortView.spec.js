ba8a23b67272f0ff64907a8d69df2734e06c47cf
Meredith Mumby <Meredith.Mumby@ca.ey.com>
2023-03-21 14:01:26 -0600

    added a common command for formatting cypress mock dates, updated tests to use that command, added test suites to cohortView, added a missing toast message to bulk graduate

--- cypress/integration/cohortView.spec.js
+++ cypress/integration/cohortView.spec.js
@@ -1,101 +1,39 @@
 describe('Tests the Cohort View', () => {
-  before(() => {
-    getCohortParticipantsFromCSV();
-    getCohorts();
-    getPSI();
-  });
-
   beforeEach(() => {
+    setAlias();
     cy.kcLogout();
   });
 
-  const handleCSVtoArr = (csvData) => {
-    const rows = csvData.split('\n');
-    const headers = rows[0].split(',');
-    const rowArr = [];
-
-    rows.slice(1).forEach((row) => {
-      const values = row.split(',');
-      const obj = {};
-
-      headers.forEach((header, index) => {
-        obj[header] = values[index];
-      });
-
-      rowArr.push(obj);
+  const setAlias = () => {
+    cy.getCSVData('psi').then((data) => {
+      cy.wrap(data).as('psi');
     });
-    return rowArr;
-  };
-
-  const getCohortParticipantsFromCSV = () => {
-    cy.readFile('server/test-data/cohort_participants.csv').then((csvData) => {
-      const result = handleCSVtoArr(csvData);
-
-      // Store the data in the Cypress test context
-      cy.wrap(result).as('cohortParticipants');
+    cy.getCSVData('cohorts').then((data) => {
+      cy.wrap(data).as('cohorts');
     });
   };
 
-  const getCohorts = () => {
-    cy.readFile('server/test-data/cohorts.csv').then((csvData) => {
-      const result = handleCSVtoArr(csvData);
-
-      // Store the data in the Cypress test context
-      cy.wrap(result).as('cohorts');
-    });
-  };
-
-  const getPSI = () => {
-    cy.readFile('server/test-data/post_secondary_institutions.csv').then((csvData) => {
-      const rows = csvData.split('\n');
-      const headers = rows[0].split(',');
-      const rowArr = [];
-
-      rows.slice(1).forEach((row) => {
-        const values = row.split(',');
-        const obj = {};
-
-        headers.forEach((header, index) => {
-          obj[header] = values[index];
-        });
-
-        rowArr.push(obj);
-      });
-      //  const result = handleCSVtoArr(csvData)
-
-      //   // Store the data in the Cypress test context
-      cy.wrap(rowArr).as('psi');
-    });
-  };
-
-  const visitsCohort = () => {
+  const visitsCohort = (healthAuthority) => {
     cy.visit('/admin');
     cy.contains('Manage PSI').click();
     cy.get('div[aria-label="Health Authority filter"]').click();
-    cy.get('li').contains('Fraser').click();
+    cy.get('li').contains(healthAuthority).click();
 
     // Click on a PSI in fraser, and click on a cohort
     cy.get('@psi').then((psi) => {
-      const pisInFraser = psi.filter((institute) => institute.health_authority === 'Fraser');
-      cy.contains(pisInFraser[0]).click();
+      cy.log(psi);
+      const pisInHA = psi.filter((institute) => institute.health_authority === healthAuthority);
+      cy.contains(pisInHA[0].institute_name).click();
       cy.get('@cohorts').then((cohorts) => {
-        cohortInPSI = cohorts.filter((cohort) => cohort.psi_id === pisInFraser[0].id);
-        cy.contains(cohortInPSI[0].cohort_name).click();
+        cy.log(cohorts);
+        const cohortInPSI = cohorts.filter((cohort) => cohort.psi_id === pisInHA[0].id);
+        // add alias for cohort to use in later in the test
+        cy.wrap(cohortInPSI[2]).as('selectedCohort');
+        // // Click on cohort 'Proper Hen Cohort'
+        cy.contains(cohortInPSI[2].cohort_name).click();
       });
     });
   };
-
-  // const visitsCohort = (cohortName) => {
-  //   cy.visit('/admin');
-  //   cy.contains('Manage PSI').click();
-  //   cy.get('div[aria-label="Health Authority filter"]').click();
-  //   cy.get('li').contains('Fraser').click();
-
-  //   cy.get('@psi').then((psi) => {
-  //   cy.contains('Full Sawfly University').click();
-  //   cy.contains(cohortName).click();
-  //   }
-  // };
 
   it('HA visits a PSI details page -> creates new cohort -> checks for no participants in the table', () => {
     cy.kcLogin('test-ha');
@@ -117,7 +55,6 @@ describe('Tests the Cohort View', () => {
     cy.get('input#cohortSize').type('100');
     cy.get('button').contains('Submit').click();
     // expect success message
-    // cy.wait(1000);
     cy.get('div.MuiAlert-message')
       .contains(`Cohort 'Empty New Cohort' added successfully`)
       .should('be.visible');
@@ -132,52 +69,94 @@ describe('Tests the Cohort View', () => {
 
   it('MOH visits cohort view, cannot `Bulk Graduate`', () => {
     cy.kcLogin('test-moh');
-    visitsCohort();
+    visitsCohort('Fraser');
 
     cy.get('.MuiTypography-subtitle1')
       .contains('No Participants in this Cohort')
       .should('be.visible');
-    // MOH can see all PARTICIPANTS
-    // cy.get('.MuiAlert-standardInfo')
-    //   .contains('Participants hired outside your region will not appear in this list')
-    //   .should('not.exist');
 
     cy.get('button').contains('Bulk Graduate').should('not.exist');
   });
 
   it('MOH visits cohort view, can see all participants in every region', () => {
     cy.kcLogin('test-moh');
-    visitsCohort();
+    visitsCohort('Fraser');
 
+    cy.get('tbody tr').should('have.length', 3);
     // MOH can see all PARTICIPANTS
     cy.get('.MuiAlert-standardInfo').should('not.exist');
   });
 
-  it('HA visits cohort view, can `Bulk Graduate`', () => {
+  it('HA visits cohort view, can only see participants in their region', () => {
     cy.kcLogin('test-ha');
-    visitsCohort();
+    visitsCohort('Fraser');
 
-    cy.get('.MuiTypography-subtitle1')
-      .contains('No Participants in this Cohort')
+    cy.get('tr td').find('[type="checkbox"]').should('have.length', 1);
+    cy.get('.MuiAlert-standardInfo')
+      .contains('Participants hired outside your region will not appear in this list')
       .should('be.visible');
+  });
+
+  const navigateToFormSelectAll = () => {
+    cy.get('th').find('[type="checkbox"]').check();
+    cy.contains('button', 'Bulk Graduate').click();
+  };
+
+  it('HA visits cohort view, can  `Bulk Graduate`', () => {
+    cy.kcLogin('test-ha');
+    visitsCohort('Fraser');
 
     cy.get('.MuiAlert-standardInfo')
       .contains('Participants hired outside your region will not appear in this list')
       .should('be.visible');
 
     cy.get('button').contains('Bulk Graduate').should('be.visible');
+
+    cy.get('tr td')
+      .find('[type="checkbox"]')
+      .its('length')
+      .then((n) => {
+        navigateToFormSelectAll();
+        cy.get('@selectedCohort').then((selectedCohort) => {
+          cy.formatDateWithOffset(selectedCohort.end_date, 1, false).then((formattedDate) => {
+            cy.get('[name=GraduationDate]').clear().type(`{ctrl+v}${formattedDate}`);
+          });
+
+          // attempt to submit form with a graduation date set before cohort endDate
+          cy.contains('button', 'Submit').should('have.class', 'Mui-disabled');
+          cy.get('.MuiAlert-standardWarning')
+            .contains('Graduation cannot be tracked before cohort has ended.')
+            .should('be.visible');
+
+          cy.formatDateWithOffset(selectedCohort.end_date, 1).then((formattedDate) => {
+            cy.get('[name=GraduationDate]').clear().type(`{ctrl+v}${formattedDate}`);
+          });
+
+          cy.contains('button', 'Submit').click();
+          // expect: no errors, success message.
+          cy.contains('.Mui-error').should('not.exist');
+          cy.get('.MuiAlert-message').contains('Participant(s) status updated');
+        });
+      });
   });
 
-  it('HA visits cohort view, can only see participants in their region', () => {
+  it('HA visits cohort view, cannot click `Bulk Graduate` as participants already have a status set', () => {
     cy.kcLogin('test-ha');
-    visitsCohort();
-
-    cy.get('.MuiTypography-subtitle1')
-      .contains('No Participants in this Cohort')
-      .should('be.visible');
+    visitsCohort('Fraser');
 
     cy.get('.MuiAlert-standardInfo')
       .contains('Participants hired outside your region will not appear in this list')
+      .should('be.visible');
+
+    cy.get('button').contains('Bulk Graduate').should('be.visible');
+    cy.get('th').find('[type="checkbox"]').check();
+
+    cy.get('button').contains('Bulk Graduate').should('have.class', 'Mui-disabled');
+
+    cy.get('.MuiAlert-standardInfo')
+      .contains(
+        'Bulk Graduation is only available for participants with no graduation status. Please deselect participants who have had a successful or unsuccessful graduation.'
+      )
       .should('be.visible');
   });
 });

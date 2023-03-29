5abf55f62047aea05500d5dfa91a2f3afd50c07c
Meredith Mumby <Meredith.Mumby@ca.ey.com>
2023-03-29 12:11:35 -0600

    updated BE create site validation to include all required

--- server/validators/employer.ts
+++ server/validators/employer.ts
@@ -222,37 +222,37 @@ export const CreateSiteSchema = yup
       .string()
       .required(errorMessage)
       .max(255, 'Site name should be no longer than 255 characters'),
-    address: yup.string().nullable(),
-    city: yup.string().nullable(),
-    isRHO: yup.boolean().nullable().required(errorMessage),
+    address: yup.string().required(errorMessage),
+    city: yup.string().required(errorMessage),
+    isRHO: yup.boolean().required(errorMessage).required(errorMessage),
     healthAuthority: yup.string().required(errorMessage).oneOf(healthRegions, 'Invalid region'),
     siteType: yup.string().required(errorMessage).oneOf(siteTypesOptions, 'Invalid site type'),
     postalCode: yup
       .string()
       .required(errorMessage)
       .matches(/^[A-Z]\d[A-Z]\s?\d[A-Z]\d$/, 'Format as A1A 1A1'),
-    registeredBusinessName: yup.string().nullable(),
-    operatorName: yup.string().nullable(),
-    operatorContactFirstName: yup.string().nullable(),
-    operatorContactLastName: yup.string().nullable(),
-    operatorEmail: yup.string().nullable().email('Invalid email address'),
+    registeredBusinessName: yup.string().required(errorMessage),
+    operatorName: yup.string().required(errorMessage),
+    operatorContactFirstName: yup.string().required(errorMessage),
+    operatorContactLastName: yup.string().required(errorMessage),
+    operatorEmail: yup.string().required(errorMessage).email('Invalid email address'),
     operatorPhone: yup
       .mixed()
-      .nullable()
+      .required(errorMessage)
       .test({
         name: 'isPhone',
         test: (value) => value === '' || !!String(value).match(/^\d{10}$/),
       }),
-    siteContactFirstName: yup.string().nullable(),
-    siteContactLastName: yup.string().nullable(),
+    siteContactFirstName: yup.string().required(errorMessage),
+    siteContactLastName: yup.string().required(errorMessage),
     siteContactPhone: yup
       .mixed()
-      .nullable()
+      .required(errorMessage)
       .test({
         name: 'isPhone',
         test: (value) => value === '' || !!String(value).match(/^\d{10}$/),
       }),
-    siteContactEmail: yup.string().nullable().email('Invalid email address'),
+    siteContactEmail: yup.string().required(errorMessage).email('Invalid email address'),
   });
 
 export const EditSiteSchema = yup

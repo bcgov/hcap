81e97bff5d91968f5dfc2feec8dc1ba34680dff8
Noel Feliciano <noel.feliciano@ca.ey.com>
2023-04-20 18:44:25 -0700

    fix: attempt at altering hidden for user ids

--- server/migrations/1681245196179_add-fk-to-participant-status.js
+++ server/migrations/1681245196179_add-fk-to-participant-status.js
@@ -1,4 +1,8 @@
 exports.up = async (pgm) => {
+  await pgm.db.query(`
+    ALTER TABLE participants_status
+    ADD COLUMN old_employer_id VARCHAR(255)
+  `);
   const { rows: users } = await pgm.db.query(`SELECT * FROM users`);
   const { rows: statuses } = await pgm.db.query(
     `SELECT DISTINCT employer_id FROM participants_status`
@@ -11,12 +15,35 @@ exports.up = async (pgm) => {
         if (user) {
           return pgm.db.query(
             `UPDATE participants_status
-             SET employer_id = ${user.id}
+             SET employer_id = ${user.id},
+             old_employer_id = '${keycloakId}'
              WHERE employer_id = '${keycloakId}'`
           );
         }
         throw Error(`user with id '${keycloakId}' not found from 'users' table`);
       })
+  );
+
+  const { rows: hiddenStatuses } = await pgm.db.query(
+    `SELECT * FROM participants_status WHERE data->>'hiddenForUserIds' IS NOT NULL`
+  );
+  await Promise.all(
+    hiddenStatuses.map((status) => {
+      const { hiddenForUserIds } = status.data;
+      const keycloakIds = Object.keys(hiddenForUserIds);
+      const usersFromKeycloakIds = keycloakIds.map((keycloakId) =>
+        users.find((u) => u.body.keycloakId === keycloakId)
+      );
+      if (usersFromKeycloakIds.length === keycloakIds.length) {
+        const userIds = usersFromKeycloakIds.map((u) => `"${u.id}": true`);
+        return pgm.db.query(
+          `UPDATE participants_status
+             SET data = jsonb_set(data, '{hiddenForUserIds}', '{${userIds.join(',')}}')
+             WHERE id = ${status.id}`
+        );
+      }
+      throw Error(`unable to edit hiddenForUserIds for status ${status.id}`);
+    })
   );
 
   await pgm.db.query(`
@@ -51,6 +78,27 @@ exports.down = async (pgm) => {
     ALTER COLUMN employer_id TYPE VARCHAR(255)`);
 
   const { rows: users } = await pgm.db.query(`SELECT * FROM users`);
+
+  const { rows: hiddenStatuses } = await pgm.db.query(
+    `SELECT * FROM participants_status WHERE data->>'hiddenForUserIds' IS NOT NULL`
+  );
+  await Promise.all(
+    hiddenStatuses.map((status) => {
+      const { hiddenForUserIds } = status.data;
+      const hiddenIds = Object.keys(hiddenForUserIds);
+      const usersFromHiddenIds = hiddenIds.map((id) => users.find((u) => u.id === +id));
+      if (usersFromHiddenIds.length === hiddenIds.length) {
+        const userIds = usersFromHiddenIds.map((u) => `"${u.body.keycloakId}": true`);
+        return pgm.db.query(
+          `UPDATE participants_status
+             SET data = jsonb_set(data, '{hiddenForUserIds}', '{${userIds.join(',')}}')
+             WHERE id = ${status.id}`
+        );
+      }
+      throw Error(`unable to edit hiddenForUserIds for status ${status.id}`);
+    })
+  );
+
   const { rows: statuses } = await pgm.db.query(
     `SELECT DISTINCT employer_id FROM participants_status`
   );
@@ -62,11 +110,16 @@ exports.down = async (pgm) => {
         if (user) {
           return pgm.db.query(
             `UPDATE participants_status
-             SET employer_id = '${user.body.keycloakId}'
+             SET employer_id = '${user.body.keycloakId}',
+             old_employer_id = NULL
              WHERE employer_id = '${id}'`
           );
         }
         throw Error(`user with id '${id}' not found from 'users' table`);
       })
   );
+  await pgm.db.query(`
+    ALTER TABLE participants_status
+    DROP COLUMN old_employer_id
+  `);
 };

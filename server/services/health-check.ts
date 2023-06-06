06e70a8fddb0172eae8214f8fc3d448d5a2d503d
Noel Feliciano <noel.feliciano@ca.ey.com>
2023-06-06 12:57:09 -0700

    fix: removing keycloak health check

--- server/services/health-check.ts
+++ server/services/health-check.ts
@@ -5,7 +5,6 @@ import logger from '../logger';
 // Script method
 export const healthCheck = async () => {
   let db = false;
-  let kc = false;
   try {
     // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
     const _ = await dbClient.db[collections.EMPLOYER_SITES].count();
@@ -17,19 +16,19 @@ export const healthCheck = async () => {
     });
   }
 
-  try {
-    await keycloak.checkHealth();
-    kc = true;
-  } catch (excp) {
-    logger.error('KC Health Check Failed!', {
-      context: 'health-check-kc',
-      error: excp,
-    });
-  }
+  // Removing for now - the Keycloak health check may not be necessary for the probes
+  // try {
+  //   await keycloak.checkHealth();
+  //   kc = true;
+  // } catch (excp) {
+  //   logger.error('KC Health Check Failed!', {
+  //     context: 'health-check-kc',
+  //     error: excp,
+  //   });
+  // }
 
   return {
     healthCheck: {
-      keycloak: kc,
       postgres: db,
     },
     version: process.env.VERSION,

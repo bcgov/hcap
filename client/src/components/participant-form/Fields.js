869314e34074f55a7784e87b98366440fe96acd6
Cory Atwater <coryatwater@xmfvfj164jq6lr.lan>
2023-09-27 04:59:00 -0400

    change third option values in radio groups from unknown

--- client/src/components/participant-form/Fields.js
+++ client/src/components/participant-form/Fields.js
@@ -238,7 +238,7 @@ export const Fields = ({
                   options={[
                     { value: 'yes', label: 'Yes' },
                     { value: 'no', label: 'No' },
-                    { value: 'unknown', label: `I don't know` },
+                    { value: 'doNotKnow', label: `I don't know` },
                   ]}
                 />
               </Grid>
@@ -307,7 +307,7 @@ export const Fields = ({
               options={[
                 { value: 'yes', label: 'Yes' },
                 { value: 'no', label: 'No' },
-                { value: 'unknown', label: 'Prefer not to answer' },
+                { value: 'preferNotToAnswer', label: 'Prefer not to answer' },
               ]}
             />
           </Grid>
@@ -349,7 +349,7 @@ export const Fields = ({
                   options={[
                     { value: 'yes', label: 'Yes' },
                     { value: 'no', label: 'No' },
-                    { value: 'unknown', label: 'Prefer not to answer' },
+                    { value: 'preferNotToAnswer', label: 'Prefer not to answer' },
                   ]}
                 />
               </Grid>

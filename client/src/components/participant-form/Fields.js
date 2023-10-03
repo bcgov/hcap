f15191e25bb75945b558b8e6df089fc914cb851d
Cory Atwater <99211385+coryatwater-FW@users.noreply.github.com>
2023-10-03 14:07:49 -0400

    try to address duplicity sonar report, extract sections into components (#985)
    * try to address duplicity sonar report, extract sections into components
    
    * move checkbox options to constants file
    
    * use formatOptions function to map options
    
    ---------
    
    Co-authored-by: Cory Atwater <coryatwater@xmfvfj164jq6lr.lan>

--- client/src/components/participant-form/Fields.js
+++ client/src/components/participant-form/Fields.js
@@ -50,14 +50,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const contactFields = [
-  { name: 'firstName', label: '4. * First Name' },
-  { name: 'lastName', label: '5. * Last Name' },
-  { name: 'phoneNumber', label: '6. * Phone Number', type: 'tel' },
-  { name: 'emailAddress', label: '7. * Email Address', type: 'email' },
-  { name: 'postalCode', label: '8. * Postal Code' },
-];
-
 export const Fields = ({
   isDisabled,
   hideHelp,
@@ -198,150 +190,13 @@ export const Fields = ({
           />
         </Grid>
 
-        {/** Q14 Background information */}
-        <SectionHeader text='Background Information' />
-        <Question
-          text='14. What industry do you currently or most recently work in? Please select the most
-              applicable option.'
-        />
-        <Grid item xs={12}>
-          <FastField
-            name='currentOrMostRecentIndustry'
-            component={RenderRadioGroup}
-            disabled={checkFieldDisability('currentOrMostRecentIndustry')}
-            setTouched
-            options={[
-              {
-                value: 'Accommodation and food services',
-                label: 'Accommodation and food services',
-              },
-              {
-                value: 'Administrative and support, waste management and remediation services ',
-                label: 'Administrative and support, waste management and remediation services ',
-              },
-              {
-                value: 'Agriculture, forestry, fishing, and hunting',
-                label: 'Agriculture, forestry, fishing, and hunting',
-              },
-              {
-                value: 'Arts, entertainment, and recreation',
-                label: 'Arts, entertainment, and recreation',
-              },
-              {
-                value: 'Community Social Services',
-                label: 'Community Social Services',
-              },
-              {
-                value: 'Construction',
-                label: 'Construction',
-              },
-              {
-                value: 'Continuing Care and Community Health Care',
-                label: 'Continuing Care and Community Health Care',
-              },
-              {
-                value: 'Educational services',
-                label: 'Educational services',
-              },
-              {
-                value: 'Finance and insurance',
-                label: 'Finance and insurance',
-              },
-              {
-                value: 'Health care and social assistance',
-                label: 'Health care and social assistance',
-              },
-              {
-                value: 'Information and cultural industries',
-                label: 'Information and cultural industries',
-              },
-              {
-                value: 'Management of companies and enterprises',
-                label: 'Management of companies and enterprises',
-              },
-              {
-                value: 'Manufacturing',
-                label: 'Manufacturing',
-              },
-              {
-                value: 'Mining, quarrying, and oil and gas extraction',
-                label: 'Mining, quarrying, and oil and gas extraction',
-              },
-              {
-                value: 'Professional, scientific, and technical services',
-                label: 'Professional, scientific, and technical services',
-              },
-              {
-                value: 'Public administration',
-                label: 'Public administration',
-              },
-              {
-                value: 'Real estate and rental and leasing',
-                label: 'Real estate and rental and leasing',
-              },
-              {
-                value: 'Retail trade',
-                label: 'Retail trade',
-              },
-              {
-                value: 'Transportation and warehousing',
-                label: 'Transportation and warehousing',
-              },
-              {
-                value: 'Tourism & Hospitality',
-                label: 'Tourism & Hospitality',
-              },
-              {
-                value: 'Utilities',
-                label: 'Utilities',
-              },
-              {
-                value: 'Wholesale trade',
-                label: 'Wholesale trade',
-              },
-              {
-                value: 'None, not working previously',
-                label: 'None, not working previously',
-              },
-              {
-                value: 'Other, please specify:',
-                label: 'Other, please specify:',
-              },
-            ]}
+          {/** Background Information Section */}
+          <BackgroundInformationSection
+            checkFieldDisability={checkFieldDisability}
+            isMHAW={values.program === 'MHAW'}
+            selectedOption={values.currentOrMostRecentIndustry}
           />
         </Grid>
-        {values.currentOrMostRecentIndustry === 'Other, please specify:' && (
-          <Grid item xs={6}>
-            <FastField
-              name='otherIndustry'
-              component={RenderTextField}
-              disabled={checkFieldDisability('otherIndustry')}
-            />
-          </Grid>
-        )}
-        {/** Q15 does/ did this rolee involve delivering mental health/ substance use services */}
-        {/** only show if Q1 is MHAW and Q14 is one of 3 below options */}
-        {values.program === 'MHAW' &&
-          (values.currentOrMostRecentIndustry === 'Health care and social assistance' ||
-            values.currentOrMostRecentIndustry === 'Continuing Care and Community Health Care' ||
-            values.currentOrMostRecentIndustry === 'Community Social Services') && (
-            <>
-              <Question
-                text='15. Does/did this role involve delivering mental health and/or substance use
-                    services?'
-              />
-              <Grid item xs={12}>
-                <FastField
-                  name='roleInvolvesMentalHealthOrSubstanceUse'
-                  component={RenderRadioGroup}
-                  disabled={checkFieldDisability('roleInvolvesMentalHealthOrSubstanceUse')}
-                  setTouched
-                  row
-                  options={YesNo}
-                />
-              </Grid>
-            </>
-          )}
 
         {/** Disclaimer and submission */}
         <Grid container spacing={2}>

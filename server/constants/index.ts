d4ebc9e5694bda694d835e10d17b85dc0ce36b5c
Sunghwan Park <sunghwan.park@ca.ey.com>
2023-10-16 16:34:30 -0700

    HCAP-1510 define a constant, MHAW_ENABLED_REGIONS

--- server/constants/index.ts
+++ server/constants/index.ts
@@ -1,5 +1,6 @@
 export * from './return-of-service';
 export * from './validation-constants';
 export * from './participant-status';
+export * from './mhaw-enabled-regions';
 export * from './milestone-report-type';
 export * from './user-roles';

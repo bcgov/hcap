70274ee38fe36dff1b0e82c219a77c04a1d7fcaa
Sunghwan Park <sunghwan.park@ca.ey.com>
2023-11-03 15:34:37 -0700

    HCAP-1517 program report entries by regions

--- server/services/reporting/program-monitoring-report.ts
+++ server/services/reporting/program-monitoring-report.ts
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import _ from 'lodash';
 import { Program } from '../../constants';
 import { dbClient, collections } from '../../db';
 
@@ -18,14 +19,20 @@ type MonitoringReportEntry = {
 
 export const getProgramMonitoringReport = async () => {
   const participants: MonitoringReportEntry[] = await dbClient.db[collections.PARTICIPANTS].find();
-  return participants.map((entry) => ({
-    participantId: entry.id,
-    created_at: dayjs(entry.created_at).format('YYYY-MM-DD'),
-    preferredLocation: entry.body?.preferredLocation,
-    experienceWithMentalHealthOrSubstanceUse: entry.body?.experienceWithMentalHealthOrSubstanceUse,
-    currentOrMostRecentIndustry: entry.body?.currentOrMostRecentIndustry,
-    reasonForFindingOut: entry.body?.reasonForFindingOut,
-    indigenous: entry.body?.indigenous,
-    program: entry.body?.program,
-  }));
+  return _.flatten(
+    participants.map((entry) => {
+      const regions = entry.body?.preferredLocation?.split(';') ?? [];
+      return regions.map((region) => ({
+        participantId: entry.id,
+        created_at: dayjs(entry.created_at).format('YYYY-MM-DD'),
+        preferredLocation: region,
+        experienceWithMentalHealthOrSubstanceUse:
+          entry.body?.experienceWithMentalHealthOrSubstanceUse,
+        currentOrMostRecentIndustry: entry.body?.currentOrMostRecentIndustry,
+        reasonForFindingOut: entry.body?.reasonForFindingOut,
+        indigenous: entry.body?.indigenous,
+        program: entry.body?.program,
+      }));
+    })
+  );
 };
